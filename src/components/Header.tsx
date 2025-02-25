941d9f1499414ae69be2eef3d7996bc81357ba38
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-02-25 15:18:42 +0530

    feat: add theme toggle functionality

---
+++ src/components/Header.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import useThemeStore from "@/store/themeStore";
+import Logo from "./Logo";
+import Switch from "./ui/switch";
+import cn from "@/utils/cn";
+
+const Header = () => {
+  const { theme, toggleTheme } = useThemeStore();
+
+  return (
+    <header className="w-full relative max-w-screen-lg flex items-center justify-between p-4">
+      <Logo />
+
+      <div className="flex items-center gap-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          className={cn(
+            "size-5 fill-none",
+            theme === "light" ? "stroke-zinc-900" : "stroke-zinc-500"
+          )}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+          />
+        </svg>
+
+        <Switch state={theme === "dark"} onClick={toggleTheme} />
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className={cn(
+            "size-[18px] fill-none",
+            theme === "dark" ? "stroke-zinc-300" : "stroke-zinc-400"
+          )}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+          />
+        </svg>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/ui/switch.tsx
@@ -0,0 +1,22 @@
+import { JSX, ButtonHTMLAttributes } from "react";
+import cn from "@/utils/cn";
+
+interface SwitchProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  state: boolean;
+}
+
+export default function Switch({ state, ...props }: SwitchProps): JSX.Element {
+  return (
+    <button
+      className="p-1 dark:bg-zinc-800 bg-zinc-200 rounded-full h-6 w-12 relative flex items-center transition-colors duration-200"
+      {...props}
+    >
+      <span
+        className={cn(
+          "size-4 block bg-teal-500 rounded-full transition-transform duration-200 ease-in-out transform",
+          state ? "translate-x-6" : "translate-x-0"
+        )}
+      ></span>
+    </button>
+  );
+}
