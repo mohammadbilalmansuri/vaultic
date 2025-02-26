ad1aa52d1ed3a16492c80010b9a76bc2ebbfb041
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-02-26 11:08:44 +0530

    feat: update header icons for improved theme responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,39 +25,41 @@ const Header = () => {
         <div className="flex items-center gap-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
             viewBox="0 0 24 24"
+            fill="none"
             strokeWidth={2}
-            className={cn(
-              "size-5 fill-none",
-              theme === "light" ? "stroke-zinc-700" : "stroke-zinc-500"
-            )}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className={cn("size-5 fill-none", {
+              "stroke-zinc-700": theme === "light",
+              "stroke-zinc-500": theme !== "light",
+            })}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-            />
+            <circle cx="12" cy="12" r="4" />
+            <path d="M12 2v2" />
+            <path d="M12 20v2" />
+            <path d="m4.93 4.93 1.41 1.41" />
+            <path d="m17.66 17.66 1.41 1.41" />
+            <path d="M2 12h2" />
+            <path d="M20 12h2" />
+            <path d="m6.34 17.66-1.41 1.41" />
+            <path d="m19.07 4.93-1.41 1.41" />
           </svg>
 
           <Switch state={theme === "dark"} onClick={toggleTheme} />
 
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
             viewBox="0 0 24 24"
             strokeWidth={2}
-            stroke="currentColor"
-            className={cn(
-              "size-[18px] fill-none",
-              theme === "dark" ? "stroke-zinc-300" : "stroke-zinc-400"
-            )}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className={cn("size-5 fill-none", {
+              "stroke-zinc-300": theme === "dark",
+              "stroke-zinc-400": theme !== "dark",
+            })}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
-            />
+            <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
           </svg>
         </div>
       </div>
