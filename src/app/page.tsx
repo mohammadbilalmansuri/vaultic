14425de0181c9d8cd8953a8b89bfcbb3767db491
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-03-03 19:38:44 +0530

    feat: add dashboard page and enhance Completion component with animations and improved UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, JSX } from "react";
 import {
   Welcome,
   SelectNetwork,
@@ -14,41 +14,31 @@ import cn from "@/utils/cn";
 export type TStep = 1 | 2 | 3 | 4 | 5 | 6;
 export type TPath = "create" | "import" | null;
 
+const TOTAL_STEPS = {
+  create: 6,
+  import: 5,
+};
+
 const Page = () => {
   const [step, setStep] = useState<TStep>(1);
   const [path, setPath] = useState<TPath>("create");
 
-  const renderStepComponent = () => {
-    switch (step) {
-      case 1:
-        return <Welcome setStep={setStep} setPath={setPath} />;
-      case 2:
-        return <SelectNetwork path={path} setStep={setStep} />;
-      case 3:
-        return path === "import" ? (
-          <ImportWallet />
-        ) : (
-          <Agreement setStep={setStep} />
-        );
-      case 4:
-        return path === "create" ? (
-          <GenerateMnemonic setStep={setStep} />
-        ) : null;
-      case 5:
-        return <CreatePassword setStep={setStep} />;
-      case 6:
-        return <Completion />;
-      default:
-        return null;
-    }
+  const stepComponents: Record<TStep, JSX.Element | null> = {
+    1: <Welcome setStep={setStep} setPath={setPath} />,
+    2: <SelectNetwork path={path} setStep={setStep} />,
+    3: path === "import" ? <ImportWallet /> : <Agreement setStep={setStep} />,
+    4: path === "create" ? <GenerateMnemonic setStep={setStep} /> : null,
+    5: <CreatePassword setStep={setStep} />,
+    6: <Completion />,
   };
 
   return (
     <div className="w-full max-w-screen-lg relative flex flex-col items-center gap-8">
-      {renderStepComponent()}
+      {stepComponents[step]}
 
+      {/* Step Indicator */}
       <div className="flex items-center gap-2">
-        {Array.from({ length: path === "create" ? 6 : 5 }, (_, index) => (
+        {Array.from({ length: TOTAL_STEPS[path ?? "create"] }, (_, index) => (
           <span
             key={index}
             className={cn("size-3 rounded-full", {
@@ -56,7 +46,7 @@ const Page = () => {
               "bg-teal-500/50": step > index + 1,
               "bg-teal-500": step === index + 1,
             })}
-          ></span>
+          />
         ))}
       </div>
     </div>
