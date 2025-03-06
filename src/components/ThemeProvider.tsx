5b73f0cf01fbe7e5054a62a8aefd5e4618540da1
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-03-06 18:22:41 +0530

    refactor: simplify ThemeProvider by removing unnecessary state and improving hydration logic

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,26 +1,19 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, ReactNode } from "react";
 import useThemeStore from "@/stores/themeStore";
 
-const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const { theme, isHydrated } = useThemeStore();
-  const [hydrated, setHydrated] = useState(false);
-
-  // here or in another route provide we have get the saved data of user if exist and load it to the store
-  // based on user status we have to prevent routes
-  // status ? user can go in dashboard but not in the home page : user can go in home page but not in the dashboard
+const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const theme = useThemeStore((state) => state.theme);
+  const isHydrated = useThemeStore((state) => state.isHydrated);
 
   useEffect(() => {
-    if (!isHydrated) {
-      setHydrated(true);
-      return;
-    }
+    if (!isHydrated) return;
     document.documentElement.className = theme;
-  }, [isHydrated, theme]);
+  }, []);
 
-  if (!hydrated) return null;
+  if (!isHydrated) return null;
 
-  return <div className="root">{children}</div>;
+  return <>{children}</>;
 };
 
 export default ThemeProvider;
