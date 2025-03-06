70a2af82fa80767baf73447132e9b013ab003b0f
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-03-06 17:50:37 +0530

    refactor: update page component to replace Agreement and GenerateMnemonic with SecretRecoveryWarning and GenerateWallet

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,7 +1,7 @@
 export { default as Welcome } from "./Welcome";
 export { default as SelectNetwork } from "./SelectNetwork";
-export { default as Agreement } from "./Agreement";
-export { default as GenerateMnemonic } from "./GenerateMnemonic";
+export { default as SecretRecoveryWarning } from "./SecretRecoveryWarning";
+export { default as GenerateWallet } from "./GenerateWallet";
 export { default as ImportWallet } from "./ImportWallet";
 export { default as CreatePassword } from "./CreatePassword";
 export { default as Completion } from "./Completion";
