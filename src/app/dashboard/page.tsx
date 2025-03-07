67cffdf1b36344dfa80c626e09e351e10b8a9dd8
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-03-07 09:25:23 +0530

    feat: update Dashboard page component to include a header

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const Page = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
 };
 
-export default page;
+export default Page;
