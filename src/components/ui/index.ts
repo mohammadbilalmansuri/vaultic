9bca1d3a0464a1536ff2f6fbe52488b4bf5c730f
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-03-08 16:26:01 +0530

    feat: export Loader component from ui index for accessibility

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -4,3 +4,4 @@ export { default as Copy } from "./copy";
 export { default as Hide } from "./hide";
 export { PasswordInput } from "./input";
 export { default as NavLink } from "./navlink";
+export { default as Loader } from "./loader";
