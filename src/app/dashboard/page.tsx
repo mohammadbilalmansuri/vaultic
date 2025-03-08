a2e96f1ab5feaceef7bb6d7abe73bb77a3350eb4
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-03-09 00:04:58 +0530

    feat: integrate AccountSettings component into dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+"use client";
+import { AccountSettings } from "@/components/dashboard";
 
 const Page = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="w-full max-w-screen-lg relative flex flex-col items-center flex-1 gap-8 py-2">
+      <AccountSettings />
     </div>
   );
 };
