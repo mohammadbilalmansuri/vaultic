bf3038f8f6fd25240d643ace11b8ca5defae8e1b
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-03-08 15:28:55 +0530

    feat: export NavLink component from ui index for accessibility

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,3 +3,4 @@ export { default as Button } from "./button";
 export { default as Copy } from "./copy";
 export { default as Hide } from "./hide";
 export { PasswordInput } from "./input";
+export { default as NavLink } from "./navlink";
