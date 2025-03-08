37458e39443ebeca2ed236143aa4b7b1f055de86
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-03-08 17:19:52 +0530

    style: adjust padding in Header and Footer components for improved layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="w-full relative flex flex-col items-center px-4 min-h-fit">
-      <div className="w-full max-w-screen-lg relative py-4 flex justify-between items-center gap-4">
+    <footer className="w-full relative flex flex-col items-center px-5 min-h-fit">
+      <div className="w-full max-w-screen-lg relative py-5 flex justify-between items-center gap-4">
         <p>&copy;2025 All Rights Reserved.</p>
         <p>
           Designed and Developed by{" "}
