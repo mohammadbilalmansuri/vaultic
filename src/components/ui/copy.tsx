623e497b0c0d0f8bceea468eb9ad1d3876e0a99f
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-03-09 07:31:29 +0530

    refactor: update SVG attributes in Copy component for consistency

--- src/components/ui/copy.tsx
+++ src/components/ui/copy.tsx
@@ -24,9 +24,9 @@ const Copy = ({
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         fill="none"
         className={cn("size-5", {
           "stroke-zinc-600 dark:stroke-zinc-400": !copied,
