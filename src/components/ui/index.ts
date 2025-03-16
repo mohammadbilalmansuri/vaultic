f47ac9e0d044fabded3e041dc4010839d3e13b8b
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-03-16 12:05:55 +0530

    refactor: update exports in ui index to include Input and Select components

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,7 +1,7 @@
 export { default as Switch } from "./switch";
 export { default as Button } from "./button";
 export { default as Copy } from "./copy";
-export { default as Hide } from "./hide";
-export { PasswordInput } from "./input";
 export { default as NavLink } from "./navlink";
 export { default as Loader } from "./loader";
+export { PasswordInput, Input } from "./input";
+export { Select, RHFSelect } from "./select";
