c490091f0637e324c0ac830e294375e9e3e07806
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-03-29 15:22:52 +0530

    feat: add useAuth hook and remove unused useAlchemy hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useCopy } from "./useCopy";
 export { default as useStorage } from "./useStorage";
 export { default as useWallet } from "./useWallet";
+export { default as useAuth } from "./useAuth";
