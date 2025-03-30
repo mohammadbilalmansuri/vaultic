26bf8f2470ec4c2355e96483d3e25fe6f5eb003b
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-03-30 17:42:12 +0530

    refactor: replace SVG icons with Warning and Lock components in SecretRecoveryWarning

--- src/components/ui/icons/index.tsx
+++ src/components/ui/icons/index.tsx
@@ -3,4 +3,6 @@ export { default as Sun } from "./sun";
 export { default as Moon } from "./moon";
 export { default as Solana } from "./solana";
 export { default as Ethereum } from "./ethereum";
+export { default as Warning } from "./warning";
+export { default as Lock } from "./lock";
 export { default as Hide } from "./hide";
