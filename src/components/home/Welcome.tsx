6b73520ef53c6851ddd10a47c856a2441866e58a
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-03-30 14:35:47 +0530

    refactor: update button variants and replace SVG with Logo component in Welcome

--- src/components/home/Welcome.tsx
+++ src/components/home/Welcome.tsx
@@ -2,6 +2,7 @@
 import { Dispatch, SetStateAction } from "react";
 import { motion } from "motion/react";
 import { Button } from "@/components/ui";
+import { Logo } from "../ui/icons";
 import { TStep, TPath } from "@/app/page";
 
 type WelcomeProps = {
@@ -20,17 +21,9 @@ const Welcome = ({ setPath, setStep }: WelcomeProps) => {
       initial={{ scale: 0.8, opacity: 0 }}
       animate={{ scale: 1, opacity: 1 }}
       transition={{ duration: 0.4, ease: "easeInOut" }}
-      exit={{ scale: 0.8, opacity: 0 }}
       className="box"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 448 512"
-        className="size-16 fill-teal-500 -mt-1"
-      >
-        <path d="M156.8 126.8c37.6 60.6 64.2 113.1 84.3 162.5-8.3 33.8-18.8 66.5-31.3 98.3-13.2-52.3-26.5-101.3-56-148.5 6.5-36.4 2.3-73.6 3-112.3zM109.3 200H16.1c-6.5 0-10.5 7.5-6.5 12.7C51.8 267 81.3 330.5 101.3 400h103.5c-16.2-69.7-38.7-133.7-82.5-193.5-3-4-8-6.5-13-6.5zm47.8-88c68.5 108 130 234.5 138.2 368H409c-12-138-68.4-265-143.2-368H157.1zm251.8-68.5c-1.8-6.8-8.2-11.5-15.2-11.5h-88.3c-5.3 0-9 5-7.8 10.3 13.2 46.5 22.3 95.5 26.5 146 48.2 86.2 79.7 178.3 90.6 270.8 15.8-60.5 25.3-133.5 25.3-203 0-73.6-12.1-145.1-31.1-212.6z" />
-      </svg>
-
+      <Logo size="lg" />
       <h3 className="text-lg text-teal-500">Welcome to Vaultic</h3>
       <h1>Let's Get Started</h1>
       <p>
@@ -40,14 +33,14 @@ const Welcome = ({ setPath, setStep }: WelcomeProps) => {
 
       <div className="flex flex-col gap-4 pt-2">
         <Button
-          variant="primary"
+          variant="teal"
           className="w-full"
           onClick={() => setState("create")}
         >
           Create a new wallet
         </Button>
         <Button
-          variant="secondary"
+          variant="zinc"
           className="w-full"
           onClick={() => setState("import")}
         >
