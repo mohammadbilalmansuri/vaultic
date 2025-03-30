7165cd58f479f8aaae325f47a7dfbba54e5091be
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-03-30 18:16:33 +0530

    feat: add Hide component to PasswordInput for toggling visibility

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,6 +1,7 @@
 export { default as Switch } from "./switch";
 export { default as Button } from "./button";
 export { default as Copy } from "./copy";
+export { default as Hide } from "./hide";
 export { default as NavLink } from "./navlink";
 export { default as Loader } from "./loader";
 export { PasswordInput, Input } from "./input";
