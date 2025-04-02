a8009ff5f8d5b04baa5efce9ac78cb97bf97542d
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-04-02 21:12:12 +0530

    refactor: remove react-toastify dependency and ToastProvider component

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -2,4 +2,3 @@ export { default as ThemeProvider } from "./ThemeProvider";
 export { default as Protected } from "./Protected";
 export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
-export { default as ToastProvider } from "./ToastProvider";
