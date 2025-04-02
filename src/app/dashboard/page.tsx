cc508003c8a2504aa3d67491324200a9ea825cd9
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-04-02 17:33:39 +0530

    replace wallet page with dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,88 +1,119 @@
 "use client";
-import { useState } from "react";
-import { motion, AnimatePresence } from "motion/react";
-import { useUserStore } from "@/stores/userStore";
-import { Input, RHFSelect, Button } from "@/components/ui";
 import { useForm, FormProvider } from "react-hook-form";
+import { useState } from "react";
+import { AnimatePresence, motion } from "motion/react";
+import { Button, Wallet, Input } from "@/components/ui";
+import { useWallet, useStorage } from "@/hooks";
+import { useWalletStore } from "@/stores/walletStore";
+import { Solana, Ethereum, Cancle } from "@/components/ui";
+import { TNetwork } from "@/stores/userStore";
 
 const Send = () => {
-  const [expanded, setExpanded] = useState(false);
+  const wallets = useWalletStore((state) => state.wallets);
+  const { createWallet } = useWallet();
+  const { saveUser } = useStorage();
+  const [isAddingNewWallet, setIsAddingNewWallet] = useState(false);
+  const [sendingFrom, setSendingFrom] = useState("");
+
   const methods = useForm();
 
+  const addWallet = async (network: TNetwork) => {
+    try {
+      await createWallet(network);
+      await saveUser();
+      setIsAddingNewWallet(false);
+    } catch (error) {
+      console.error("Error creating wallet:", error);
+    }
+  };
+
   return (
-    <div className="w-full max-w-screen-lg relative flex flex-col items-center flex-1 gap-5 py-2">
-      <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.4, delay: 0.3, ease: "easeOut" }}
-        className="w-full border-2 border-color rounded-xl relative p-5 flex flex-col cursor-pointer"
-        onClick={() => {
-          if (expanded) return;
-          setExpanded(true);
-        }}
-      >
-        <div className="w-full flex items-center justify-between">
-          <h2 className="text-2xl heading-color">Send</h2>
+    <>
+      <div className="w-full max-w-screen-lg relative flex flex-col items-center flex-1 gap-5 py-5">
+        <div className="w-full flex flex-col gap-5">
+          <div className="w-full flex items-center justify-between">
+            <h2 className="text-2xl heading-color">Your Wallets</h2>
 
-          <button className="icon" onClick={() => setExpanded((prev) => !prev)}>
-            <motion.svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              animate={{ rotate: expanded ? 180 : 0 }}
-              transition={{ duration: 0.4 }}
-            >
-              <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
-            </motion.svg>
-          </button>
-        </div>
-
-        <AnimatePresence>
-          {expanded && (
-            <motion.div
-              initial={{ opacity: 0, height: 0 }}
-              animate={{ opacity: 1, height: "auto" }}
-              exit={{ opacity: 0, height: 0 }}
-              transition={{ duration: 0.4, ease: "easeInOut" }}
-              className="overflow-hidden w-full relative flex flex-col gap-3"
-            >
-              <FormProvider {...methods}>
-                <form
-                  onSubmit={methods.handleSubmit((data) => console.log(data))}
-                  className="mt-6 w-full grid grid-cols-2 gap-4"
+            <div className="flex items-center gap-3">
+              {isAddingNewWallet ? (
+                <>
+                  <Cancle
+                    className="size-10 cursor-pointer icon"
+                    onClick={() => setIsAddingNewWallet(false)}
+                  />
+                  <Solana
+                    className="size-10 bg-zinc-200 dark:bg-zinc-800 p-2.5 rounded-xl cursor-pointer"
+                    onClick={() => addWallet("solana")}
+                  />
+                  <Ethereum
+                    className="size-10 bg-zinc-200 dark:bg-zinc-800 p-2 rounded-xl cursor-pointer"
+                    onClick={() => addWallet("ethereum")}
+                  />
+                </>
+              ) : (
+                <Button
+                  className="h-10 px-4"
+                  onClick={() => setIsAddingNewWallet(true)}
                 >
-                  <RHFSelect
-                    control={methods.control}
-                    name="network-select"
-                    options={[
-                      { label: "Solana", value: "1" },
-                      { label: "Ethereum", value: "2" },
-                    ]}
-                    placeholder="Select network"
-                  />
-                  <RHFSelect
-                    control={methods.control}
-                    name="wallet-select"
-                    options={[
-                      { label: "Wallet 1", value: "1" },
-                      { label: "Wallet 2", value: "2" },
-                    ]}
-                    placeholder="Select wallet"
-                  />
+                  Add new wallet
+                </Button>
+              )}
+            </div>
+          </div>
 
-                  <Input placeholder="Enter address" />
-                  <div className="w-full flex items-center gap-4">
-                    <Input placeholder="Enter amount" className="w-3/4" />
-                    <Button type="submit" className="w-1/4">
-                      Send
-                    </Button>
-                  </div>
-                </form>
-              </FormProvider>
-            </motion.div>
-          )}
-        </AnimatePresence>
-      </motion.div>
-    </div>
+          <AnimatePresence>
+            <div className="w-full flex flex-col gap-5">
+              {wallets.map((wallet, index) => (
+                <motion.div
+                  key={wallet.address}
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{
+                    duration: 0.3,
+                    delay: index * 0.1,
+                  }}
+                  className="w-full"
+                >
+                  <Wallet
+                    {...wallet}
+                    name={`Wallet ${index + 1}`}
+                    isSingle={wallets.length === 1}
+                    setSendingFrom={setSendingFrom}
+                  />
+                </motion.div>
+              ))}
+            </div>
+          </AnimatePresence>
+        </div>
+      </div>
+
+      {sendingFrom && (
+        <div className="size-full absolute flex flex-col items-center justify-center flex-1 gap-5 bg-zinc-200/60 dark:bg-zinc-800/50 backdrop-blur-sm rounded-2xl p-5">
+          <div className="w-full max-w-xl relative flex flex-col gap-5 py-5">
+            <h2 className="text-2xl heading-color">Send</h2>
+            <p>Sending from {sendingFrom}</p>
+
+            <FormProvider {...methods}>
+              <form
+                onSubmit={methods.handleSubmit((data) => console.log(data))}
+                className="w-full flex flex-col gap-5"
+              >
+                <Input placeholder="Enter address" />
+                <Input placeholder="Enter amount" />
+                <Button type="submit">Send</Button>
+                <Button
+                  variant="zinc"
+                  type="button"
+                  onClick={() => setSendingFrom("")}
+                >
+                  cancle
+                </Button>
+              </form>
+            </FormProvider>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
