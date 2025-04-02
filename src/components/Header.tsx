e9f140842beecf8bb3df3b2510b1c4da8fcf160c
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-04-02 08:32:34 +0530

    refactor: conditionally render navigation links based on user authentication status in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,12 @@ import { Switch, NavLink } from "@/components/ui";
 import { Logo, Moon, Sun } from "./ui/icons";
 import cn from "@/utils/cn";
 import { usePathname } from "next/navigation";
+import { useUserStore } from "@/stores/userStore";
 
 const Header = () => {
   const { theme, toggleTheme } = useThemeStore();
   const pathname = usePathname();
+  const authenticated = useUserStore((state) => state.authenticated);
 
   return (
     <header className="w-full relative flex flex-col items-center px-5 min-h-fit">
@@ -20,15 +22,16 @@ const Header = () => {
         </div>
 
         <nav className="flex items-center gap-8">
-          <NavLink href="/dashboard" active={pathname === "/dashboard"}>
-            Dashboard
-          </NavLink>
-          <NavLink href="/wallets" active={pathname === "/wallets"}>
-            Wallets
-          </NavLink>
-          <NavLink href="/account" active={pathname === "/account"}>
-            Account
-          </NavLink>
+          {authenticated && (
+            <>
+              <NavLink href="/dashboard" active={pathname === "/dashboard"}>
+                Dashboard
+              </NavLink>
+              <NavLink href="/account" active={pathname === "/account"}>
+                Account
+              </NavLink>
+            </>
+          )}
 
           <div className="flex items-center gap-2">
             <Sun
