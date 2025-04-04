b24828379d4e81d0644c4ff2c0687283e955a311
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-04-04 07:23:55 +0530

    fix: remove duplicate export of useAuth in hooks index

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,4 @@
+export { default as useAuth } from "./useAuth";
 export { default as useCopy } from "./useCopy";
 export { default as useStorage } from "./useStorage";
 export { default as useWallet } from "./useWallet";
-export { default as useAuth } from "./useAuth";
