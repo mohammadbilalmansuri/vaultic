9b31246bfd2392ca9340090370d674855c87da55
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-04-04 19:21:06 +0530

    feat: add loading indicator during logout process in Account component

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { Button } from "@/components/common";
+import { useState } from "react";
+import { Button, Loader } from "@/components/common";
 import { useStorage, useCopy } from "@/hooks";
 import { useRouter } from "next/navigation";
 import { useUserStore } from "@/stores/userStore";
@@ -10,14 +11,17 @@ const Account = () => {
   const { copyToClipboard, copied } = useCopy();
   const { removeUser } = useStorage();
   const router = useRouter();
+  const [loggingOut, setLoggingOut] = useState(false);
 
   const logout = async () => {
+    setLoggingOut(true);
     try {
-      router.push("/");
-      await new Promise((resolve) => setTimeout(resolve, 50));
       await removeUser();
+      router.push("/");
     } catch (error) {
       console.error("Error removing user:", error);
+    } finally {
+      setTimeout(() => setLoggingOut(false), 500);
     }
   };
 
@@ -32,30 +36,36 @@ const Account = () => {
           </Button>
         </div>
 
-        <div
-          className="w-full bg-zinc-200/60 dark:bg-zinc-800/50 hover:bg-zinc-200 dark:hover:bg-zinc-800 rounded-2xl flex flex-col px-5 pt-5 gap-5 cursor-pointer transition-all duration-300"
-          onClick={() => copyToClipboard(mnemonic)}
-        >
-          <div className="w-full grid grid-cols-6 gap-4">
-            {mnemonic.split(" ").map((word, index) => (
-              <div key={index} className="flex items-center gap-2">
-                <span className="opacity-80">{index + 1}.</span>
-                <span className="lowercase heading-color">{word}</span>
-              </div>
-            ))}
+        {loggingOut ? (
+          <div className="w-full flex items-center justify-center">
+            <Loader size="md" />
           </div>
-
-          <p
-            className={cn(
-              "text-sm leading-none py-4 border-t border-zinc-300/80 dark:border-zinc-700/60 text-center",
-              {
-                "text-teal-500": copied,
-              }
-            )}
+        ) : (
+          <div
+            className="w-full bg-zinc-200/60 dark:bg-zinc-800/50 hover:bg-zinc-200 dark:hover:bg-zinc-800 rounded-2xl flex flex-col px-5 pt-5 gap-5 cursor-pointer transition-all duration-300"
+            onClick={() => copyToClipboard(mnemonic)}
           >
-            {copied ? "Copied" : "Click anywhere on this card to copy"}
-          </p>
-        </div>
+            <div className="w-full grid grid-cols-6 gap-4">
+              {mnemonic.split(" ").map((word, index) => (
+                <div key={index} className="flex items-center gap-2">
+                  <span className="opacity-80">{index + 1}.</span>
+                  <span className="lowercase heading-color">{word}</span>
+                </div>
+              ))}
+            </div>
+
+            <p
+              className={cn(
+                "text-sm leading-none py-4 border-t border-zinc-300/80 dark:border-zinc-700/60 text-center",
+                {
+                  "text-teal-500": copied,
+                }
+              )}
+            >
+              {copied ? "Copied" : "Click anywhere on this card to copy"}
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
