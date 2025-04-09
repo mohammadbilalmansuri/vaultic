9b4d4b8798a0600ba55bdb2da59466629ea26ac2
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-04-09 21:03:09 +0530

    feat: add getRpcUrl utility for retrieving RPC URLs based on network and mode

---
+++ src/utils/getRpcUrl.ts
@@ -0,0 +1,24 @@
+import { useUserStore, TNetwork } from "@/stores/userStore";
+
+const rpcUrls = {
+  ethereum: {
+    mainnet: process.env.NEXT_PUBLIC_ETH_MAINNET_RPC!,
+    devnet: process.env.NEXT_PUBLIC_ETH_SEPOLIA_RPC!,
+  },
+  solana: {
+    mainnet: process.env.NEXT_PUBLIC_SOLANA_MAINNET_RPC!,
+    devnet: process.env.NEXT_PUBLIC_SOLANA_DEVNET_RPC!,
+  },
+};
+
+export const getRpcUrl = (network: TNetwork): string => {
+  const { networkMode } = useUserStore.getState();
+
+  const url = rpcUrls[network][networkMode];
+
+  if (!url) {
+    throw new Error(`Unsupported network or mode: ${network} - ${networkMode}`);
+  }
+
+  return url;
+};
