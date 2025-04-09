3edfbccd9beab44d73065f89976acd1432beddfd
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-04-09 20:39:16 +0530

    refactor: remove unused conversion functions for wei and sol

--- src/utils/format.ts
+++
@@ -1,4 +0,0 @@
-export const weiToEth = (wei: bigint) => Number(wei) / 1e18;
-export const ethToWei = (eth: number) => BigInt(eth * 1e18);
-export const lamportsToSol = (lamports: number) => lamports / 1e9;
-export const solToLamports = (sol: number) => sol * 1e9;
