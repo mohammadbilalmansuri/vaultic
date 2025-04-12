1a5a58f748e9632fa307eee52e354aaf4ec25788
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-04-12 22:00:04 +0530

    refactor: enhance copyToClipboard function with feedback and error handling improvements

--- src/hooks/useCopy.ts
+++ src/hooks/useCopy.ts
@@ -1,24 +1,41 @@
 "use client";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import useNotificationStore from "@/stores/notificationStore";
 
 const useCopy = () => {
+  const notify = useNotificationStore((state) => state.notify);
   const [copied, setCopied] = useState(false);
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
-  const copyToClipboard = async (text: string) => {
+  const copyToClipboard = async (
+    text: string,
+    feedback = false
+  ): Promise<boolean> => {
     if (!navigator?.clipboard?.writeText) {
-      console.warn("Clipboard API not available");
-      return;
+      notify("Clipboard not supported in this browser.", "error");
+      return false;
     }
 
-    if (copied) return;
+    if (copied) return false;
 
     try {
       await navigator.clipboard.writeText(text);
       setCopied(true);
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-      setCopied(false);
+
+      if (feedback) notify("Copied to clipboard!", "success");
+
+      if (timeoutRef.current) clearTimeout(timeoutRef.current);
+
+      timeoutRef.current = setTimeout(() => {
+        setCopied(false);
+        timeoutRef.current = null;
+      }, 2000);
+
+      return true;
     } catch (error) {
       console.error("Failed to copy text:", error);
+      notify("Oops! Something went wrong while copying.", "error");
+      return false;
     }
   };
 
