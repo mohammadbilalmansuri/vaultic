3b73f5479fbb842ab3a8956f90d1755b42b520d0
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-04-12 20:58:33 +0530

    refactor: remove comments

--- src/utils/appError.ts
+++ src/utils/appError.ts
@@ -1,13 +1,5 @@
-/**
- * Custom error class used throughout the app to separate user-friendly messages from internal technical details.
- *
- * Useful for error logging while keeping clean UX in toasts, modals, or banners.
- */
-
 class AppError extends Error {
-  // Safe message to display to the user (in toasts, UIs, etc.)
   userMessage: string;
-
   constructor(userMessage: string, technicalDetails?: string) {
     super(technicalDetails ?? userMessage);
     this.name = "AppError";
