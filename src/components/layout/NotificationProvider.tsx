c0e4ef056ba5ebe6cdcdad204635dfc9926d3888
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-04-15 22:17:24 +0530

    refactor: streamline notification rendering with icon mapping and improve styling

--- src/components/layout/NotificationProvider.tsx
+++ src/components/layout/NotificationProvider.tsx
@@ -1,7 +1,13 @@
 "use client";
 import useNotificationStore from "@/stores/notificationStore";
 import { motion, AnimatePresence } from "motion/react";
-import { Cancel, Info, Success, Error } from "../icons";
+import { Cancel, Info, Success, Error } from "@/components/icons";
+
+const NOTIFICATION_ICONS = new Map([
+  ["info", <Info className="w-6 fill-zinc-800 dark:fill-zinc-200" />],
+  ["success", <Success className="w-6 fill-teal-500" />],
+  ["error", <Error className="w-6 fill-rose-500" />],
+]);
 
 const NotificationProvider = () => {
   const opened = useNotificationStore((state) => state.opened);
@@ -15,24 +21,25 @@ const NotificationProvider = () => {
     <AnimatePresence mode="wait">
       {opened && (
         <motion.div
-          key={message}
           initial={{ opacity: 0, y: 40 }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: -40 }}
           transition={{ duration: 0.2, ease: "easeInOut" }}
-          className="max-w-lg fixed bottom-8 right-8 z-50 bg-zinc-200 dark:bg-zinc-800 heading-color p-5 rounded-2xl flex items-center gap-4 border-1.5 border-zinc-300/50 dark:border-zinc-700/50"
+          className="max-w-lg fixed bottom-8 right-8 z-50 backdrop-blur-xl p-5 rounded-2xl flex items-center justify-between gap-4 border-1.5 border-color shadow-xl"
         >
-          <div className="flex items-center gap-2">
-            {type === "info" && (
-              <Info className="w-6 fill-zinc-800 dark:fill-zinc-200" />
-            )}
-            {type === "success" && <Success className="w-6 fill-teal-500" />}
-            {type === "error" && <Error className="w-6 fill-red-500" />}
-            <span>{message}</span>
+          <div className="flex items-center gap-2.5">
+            <span className="min-w-fit">
+              {NOTIFICATION_ICONS.get(type || "info")}
+            </span>
+            <p className="text-zinc-800 dark:text-zinc-200 leading-tight">
+              {message}
+            </p>
           </div>
+
           <Cancel
-            className="min-w-fit w-3.5 fill-zinc-600 dark:fill-zinc-600 hover:fill-zinc-800 dark:hover:fill-zinc-200 transition-all duration-300"
+            className="min-w-fit fill-zinc-500 hover:fill-zinc-800 dark:hover:fill-zinc-200 transition-all duration-300"
             onClick={closeNotification}
+            svgProps={{ className: "w-3.5" }}
           />
         </motion.div>
       )}
