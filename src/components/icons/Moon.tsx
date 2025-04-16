37bc78e118b54fa420afa6abe8e228971abcbd18
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-04-16 06:56:55 +0530

    refactor: remove className prop from Moon and Sun components

--- src/components/icons/Moon.tsx
+++ src/components/icons/Moon.tsx
@@ -1,15 +1,14 @@
 import { SVGProps } from "react";
 
-const Moon = ({ className = "", ...props }: SVGProps<SVGSVGElement>) => {
+const Moon = ({ ...props }: SVGProps<SVGSVGElement>) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      strokeWidth={2}
+      strokeWidth="2"
       fill="none"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className={className}
       {...props}
     >
       <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
--- src/components/icons/Sun.tsx
+++ src/components/icons/Sun.tsx
@@ -1,15 +1,14 @@
 import { SVGProps } from "react";
 
-const Sun = ({ className = "", ...props }: SVGProps<SVGSVGElement>) => {
+const Sun = ({ ...props }: SVGProps<SVGSVGElement>) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="none"
-      strokeWidth={2}
+      strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className={className}
       {...props}
     >
       <circle cx="12" cy="12" r="4" />
