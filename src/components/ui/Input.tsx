bf934256439f7aeda257e49fdb31168203a5b552
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-04-17 08:31:18 +0530

    refactor: convert Input component to use a standard function instead of forwardRef for improved clarity

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,20 +1,25 @@
 "use client";
-import { forwardRef, ComponentProps } from "react";
+import { ComponentProps, Ref } from "react";
 import cn from "@/utils/cn";
 
-const Input = forwardRef<HTMLInputElement, ComponentProps<"input">>(
-  ({ type = "text", placeholder, className = "", ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        placeholder={placeholder}
-        className={cn("input", className)}
-        ref={ref}
-        {...props}
-      />
-    );
-  }
-);
+interface InputProps extends ComponentProps<"input"> {
+  ref: Ref<HTMLInputElement>;
+}
+
+const Input = ({
+  type = "text",
+  className = "",
+  ref,
+  ...props
+}: InputProps) => {
+  return (
+    <input
+      type={type}
+      className={cn("input", className)}
+      ref={ref}
+      {...props}
+    />
+  );
+};
 
 export default Input;
-Input.displayName = "Input";
