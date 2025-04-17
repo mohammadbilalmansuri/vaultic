d6668935eeb8fd2afd43060bdaad50985aed44f3
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-04-18 02:21:23 +0530

    refactor: simplify Completion component by removing unused hooks and updating text

--- src/components/onboarding/Completion.tsx
+++ src/components/onboarding/Completion.tsx
@@ -2,13 +2,8 @@
 import { motion } from "motion/react";
 import { Button } from "@/components/ui";
 import { Logo } from "@/components/ui/icons";
-import { useRouter } from "next/navigation";
-import useNotificationStore from "@/stores/notificationStore";
 
 const Completion = () => {
-  const router = useRouter();
-  const notify = useNotificationStore((state) => state.notify);
-
   return (
     <motion.div
       initial={{ scale: 0.8, opacity: 0 }}
@@ -18,22 +13,13 @@ const Completion = () => {
     >
       <Logo className="-mt-1 w-15 fill-teal-500" />
       <h3 className="text-lg text-teal-500">Congratulations</h3>
-      <h1>You're all good!</h1>
+      <h1>You're all ready!</h1>
       <p className="max-w-xs">
         Keep your Secret Recovery Phrase in a safe place. If you lose it, no one
         can recover it for you, and you will permanently lose access to your
         wallet.
       </p>
-      <Button
-        className="mt-3"
-        onClick={() => {
-          notify({
-            type: "info",
-            message: "Wallet setup completed successfully!",
-          });
-          router.push("/dashboard");
-        }}
-      >
+      <Button as="link" href="/dashboard" className="mt-3">
         Go to Dashboard
       </Button>
     </motion.div>
