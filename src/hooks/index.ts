6324ec0507b4426236db3a9d74d4ec5a93af9737
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-04-17 17:33:53 +0530

    refactor: replace useCopy hook with useClipboard for improved clarity and consistency

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,5 @@
 export { default as useStorage } from "./useStorage";
 export { default as useAuth } from "./useAuth";
-export { default as useCopy } from "./useCopy";
+export { default as useClipboard } from "./useClipboard";
 export { default as useWallet } from "./useWallet";
 export { default as useBlockchain } from "./useBlockchain";
