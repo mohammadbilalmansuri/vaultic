8c8525df0fe4ea3b5f0ec4a3ea1dbabfd87ab9d7
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-04-23 06:22:18 +0530

    fix: remove Select component from UI exports to streamline imports

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,5 +3,4 @@ export { default as Input } from "./Input";
 export { default as Loader } from "./Loader";
 export { default as NavLink } from "./NavLink";
 export { default as PasswordInput } from "./PasswordInput";
-export { default as Select } from "./Select";
 export { default as Switch } from "./Switch";
