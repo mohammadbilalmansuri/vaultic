3c27aa5f5b4c1e946b056f5a8ca1a894a5bf0187
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-04-23 19:27:18 +0530

    fix: simplify rendering logic in TestnetNotice component for clarity

--- src/components/layout/TestnetNotice.tsx
+++ src/components/layout/TestnetNotice.tsx
@@ -3,13 +3,14 @@ import useUserStore from "@/stores/userStore";
 
 const TestnetNotice = () => {
   const networkMode = useUserStore((state) => state.networkMode);
+  const userExists = useUserStore((state) => state.userExists);
+
+  if (!userExists || networkMode !== "devnet") return null;
 
   return (
-    networkMode === "devnet" && (
-      <p className="w-full heading-color px-5 py-2 flex items-center justify-center gap-2 bg-yellow-500/10 border-1.5 border-yellow-500/10">
-        You are currently in testnet mode.
-      </p>
-    )
+    <p className="w-full heading-color px-5 py-2 flex items-center justify-center gap-2 bg-yellow-500/10 border-1.5 border-yellow-500/10">
+      You are currently in testnet mode.
+    </p>
   );
 };
 
