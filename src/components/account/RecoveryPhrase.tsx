ab0a485fcc1cecacc0dd11d044854af91dee358e
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-04-24 20:04:20 +0530

    fix: refactor RecoveryPhrase component for improved structure and clarity

--- src/components/account/RecoveryPhrase.tsx
+++ src/components/account/RecoveryPhrase.tsx
@@ -1,47 +1,28 @@
 "use client";
 import { useState } from "react";
-import { Copy, Hide } from "@/components/ui/icons";
 import { useClipboard } from "@/hooks";
 import useUserStore from "@/stores/userStore";
 import cn from "@/utils/cn";
+import { MnemonicView } from "@/components/common";
 
-export default function RecoveryPhrase() {
-  const mnemonic = useUserStore((state) => state.mnemonic);
-  const copyToClipboard = useClipboard();
-
-  const [copied, setCopied] = useState(false);
-  const [hidden, setHidden] = useState(true);
-
+const RecoveryPhrase = () => {
   return (
-    <div className="w-full relative bg-zinc-200/60 dark:bg-zinc-800/50 rounded-xl flex flex-col overflow-hidden">
-      <div className="bg-zinc-200 dark:bg-zinc-800 flex items-center justify-between py-3 px-4 rounded-xl">
-        <p className="leading-none">Save these words in a safe place.</p>
-        <div className="flex items-center gap-4">
-          <Hide hidden={hidden} onClick={() => setHidden((prev) => !prev)} />
-          <Copy
-            copied={copied}
-            onClick={() => copyToClipboard(mnemonic, copied, setCopied)}
-          />
-        </div>
-      </div>
+    <div className="w-full relative flex gap-4">
+      <MnemonicView widthClassName="w-2/3" cols={4} />
 
-      <div
-        className="w-full grid grid-cols-2 xs:grid-cols-3 gap-4 cursor-pointer p-4"
-        onClick={() => copyToClipboard(mnemonic, copied, setCopied)}
-      >
-        {mnemonic.split(" ").map((word, index) => (
-          <div key={index} className="flex items-center gap-2">
-            <span className="opacity-80">{index + 1}.</span>
-            <span
-              className={cn("lowercase heading-color", {
-                "tracking-[0.2em]": hidden,
-              })}
-            >
-              {hidden ? Array(word.length).fill("•").join("") : word}
-            </span>
-          </div>
-        ))}
+      <div className="w-1/3 flex flex-col items-center justify-center text-center gap-2 p-4 rounded-2xl bg-warning">
+        <p>
+          You're viewing your recovery phrase.{" "}
+          <strong>Do not share it with anyone.</strong>
+        </p>
+
+        <p>
+          This phrase gives full access to your wallet — keep it private and
+          secure.
+        </p>
       </div>
     </div>
   );
-}
+};
+
+export default RecoveryPhrase;
