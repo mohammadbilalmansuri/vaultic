d92ad97e136945af12336097dbe237ea762c8a40
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-04-24 09:46:30 +0530

    fix: remove extra space in copyright notice in Footer component

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="w-full relative flex flex-col items-center px-5 min-h-fit">
       <div className="w-full max-w-screen-lg relative py-5 flex justify-between items-center gap-4">
-        <p>&copy; 2025 Vaultic. All rights reserved.</p>
+        <p>&copy;2025 Vaultic. All rights reserved.</p>
         <p>
           Designed and Developed by{" "}
           <Link
