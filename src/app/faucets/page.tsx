a82eb5ca974cae61d6d2f0c1968905b96b5eee37
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-04-27 19:23:34 +0530

    feat: implement Faucets component with network selection for airdrop tokens

--- src/app/faucets/page.tsx
+++ src/app/faucets/page.tsx
@@ -1,5 +1,45 @@
+"use client";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { Button } from "@/components/ui";
+import { Logo, Solana, Ethereum } from "@/components/ui/icons";
+
 const Faucets = () => {
-  return <div>page</div>;
+  const [faucet, setFaucet] = useState<"solana" | "ethereum" | null>(null);
+
+  return (
+    <motion.div
+      initial={{ scale: 0.8, opacity: 0 }}
+      animate={{ scale: 1, opacity: 1 }}
+      transition={{ duration: 0.4, ease: "easeInOut" }}
+      className="box"
+    >
+      <h1>Available Faucets</h1>
+      <p className="-mt-1">On which network do you want airdrop test tokens</p>
+
+      <div className="flex flex-col gap-4 pt-2">
+        <Button
+          variant="zinc"
+          className="gap-2.5"
+          onClick={() => setFaucet("solana")}
+        >
+          <Solana className="h-4 min-w-fit" />
+          <span className="mt-px">Solana Devnet</span>
+        </Button>
+
+        <Button
+          variant="zinc"
+          as="link"
+          href="https://cloud.google.com/application/web3/faucet/ethereum/sepolia"
+          target="_blank"
+          className="gap-2.5"
+        >
+          <Ethereum className="h-6 min-w-fit" />
+          <span className="mt-px">Ethereum Sepolia</span>
+        </Button>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Faucets;
