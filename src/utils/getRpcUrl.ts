c9b20be1a6b37499eed3d245a8d0bfbceca5d8be
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-04-27 15:08:57 +0530

    feat: enhance getRpcUrl function to accept network mode as an optional parameter

--- src/utils/getRpcUrl.ts
+++ src/utils/getRpcUrl.ts
@@ -1,13 +1,15 @@
 import useUserStore from "@/stores/userStore";
-import { TNetwork } from "@/types";
+import { TNetwork, TNetworkMode } from "@/types";
 import { RPC_URLs } from "@/constants";
 
-const getRpcUrl = (network: TNetwork): string => {
-  const { networkMode } = useUserStore.getState();
+const getRpcUrl = (network: TNetwork, mode?: TNetworkMode): string => {
+  const networkMode = mode ?? useUserStore.getState().networkMode;
   const url = RPC_URLs[network]?.[networkMode];
+
   if (!url) {
     throw new Error("RPC URL not found for the specified network and mode");
   }
+
   return url;
 };
 
