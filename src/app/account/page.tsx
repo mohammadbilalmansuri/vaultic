137d7d6860529888c7212350d4d4b7ca7c113cb1
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-04-27 14:47:45 +0530

    feat: update Account component to use Testnet Mode and improve layout

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -4,7 +4,7 @@ import { motion, AnimatePresence } from "motion/react";
 import {
   RecoveryPhrase,
   ChangePassword,
-  NetworkSettings,
+  TestnetMode,
   RemoveAccount,
 } from "@/components/account";
 import cn from "@/utils/cn";
@@ -12,16 +12,16 @@ import cn from "@/utils/cn";
 const TABS = new Map<string, JSX.Element>([
   ["Recovery Phrase", <RecoveryPhrase />],
   ["Change Password", <ChangePassword />],
-  ["Network Settings", <NetworkSettings />],
+  ["Testnet Mode", <TestnetMode />],
   ["Remove Account", <RemoveAccount />],
 ]);
 
 const Account = () => {
-  const [tab, setTab] = useState<string>("Recovery Phrase");
+  const [tab, setTab] = useState<string>("Testnet Mode");
 
   return (
     <div className="w-full max-w-screen-lg relative flex flex-col flex-1 py-5">
-      <div className="overflow-hidden w-full relative flex flex-col gap-4 border-2 border-color p-5 rounded-3xl">
+      <div className="w-full relative flex flex-col gap-5 border-2 border-color p-5 rounded-3xl">
         <div className="w-full relative grid grid-cols-4 bg-1 rounded-2xl p-1 h-14">
           <motion.div
             className="absolute bg-2 rounded-xl h-[calc(100%-8px)] top-1"
@@ -50,7 +50,10 @@ const Account = () => {
         </div>
 
         <AnimatePresence mode="wait">
-          <div className="w-full relative flex flex-col" key={tab}>
+          <div
+            className="w-full relative flex flex-col items-center p-3"
+            key={tab}
+          >
             {TABS.get(tab)}
           </div>
         </AnimatePresence>
