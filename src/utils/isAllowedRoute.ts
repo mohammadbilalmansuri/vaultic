43ca92432a48135a6e38998a998f16b8e059bb5e
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-04-30 11:31:51 +0530

    refactor: remove isAllowedRoute utility function and its dependencies

--- src/utils/isAllowedRoute.ts
+++
@@ -1,9 +0,0 @@
-import { RouteState } from "@/types";
-import { ROUTES } from "@/constants";
-
-const isAllowedRoute = (state: RouteState, pathname: string): boolean => {
-  const allowedPaths = ROUTES.get(state);
-  return allowedPaths?.has(pathname) ?? false;
-};
-
-export default isAllowedRoute;
