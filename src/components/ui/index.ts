74d8a78a946101bf072500c1e92134445a13d4a0
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-04-30 11:47:50 +0530

    refactor: remove duplicate NavLink export in index.ts

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,7 +1,9 @@
+export { default as ThemeSwitcher } from "./ThemeSwitcher";
+export { default as NavLink } from "./NavLink";
+
 export { default as Button } from "./Button";
 export { default as Input } from "./Input";
 export { default as Loader } from "./Loader";
-export { default as NavLink } from "./NavLink";
 export { default as PasswordInput } from "./PasswordInput";
 export { default as Switch } from "./Switch";
 export { default as CustomInputSelect } from "./CustomInputSelect";
