90c9c14b9a5c61cd14465a5b80f6bfcc378916fc
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-05-02 08:31:21 +0530

    refactor: replace HowItWorks component with WhyVaultic in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { Hero, Features, HowItWorks } from "@/components/home";
+import { Hero, Features, WhyVaultic } from "@/components/home";
 
 const Home = () => {
   return (
     <div className="w-full max-w-screen-lg relative flex-1 flex flex-col items-center gap-8 py-5">
       <Hero />
       <Features />
-      <HowItWorks />
+      <WhyVaultic />
     </div>
   );
 };
