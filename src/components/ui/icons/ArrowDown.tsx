d931688c08ac23e9095d27d013fee5b9fd68dce2
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-05-03 14:28:29 +0530

    fix: correct stroke property attributes in ArrowDown icon component for consistency

--- src/components/ui/icons/ArrowDown.tsx
+++ src/components/ui/icons/ArrowDown.tsx
@@ -7,10 +7,10 @@ const ArrowDown = ({ ...props }: SVGProps<SVGSVGElement>) => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-miterlimit="10"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...props}
     >
       <path d="M19.9201 8.94995L13.4001 15.47C12.6301 16.24 11.3701 16.24 10.6001 15.47L4.08008 8.94995" />
