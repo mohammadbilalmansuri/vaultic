d31bed7b646e66029e3c2b1acbecad8594dc13f4
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-05-03 14:58:46 +0530

    refactor: update Lock icon component for improved consistency and styling

--- src/components/ui/icons/Lock.tsx
+++ src/components/ui/icons/Lock.tsx
@@ -2,8 +2,21 @@ import { SVGProps } from "react";
 
 const Lock = ({ ...props }: SVGProps<SVGSVGElement>) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" {...props}>
-      <path d="M144 144l0 48 160 0 0-48c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192l0-48C80 64.5 144.5 0 224 0s144 64.5 144 144l0 48 16 0c35.3 0 64 28.7 64 64l0 192c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 256c0-35.3 28.7-64 64-64l16 0z" />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M6 10V8C6 4.69 7 2 12 2C17 2 18 4.69 18 8V10" />
+      <path d="M17 22H7C3 22 2 21 2 17V15C2 11 3 10 7 10H17C21 10 22 11 22 15V17C22 21 21 22 17 22Z" />
+      <path d="M15.9965 16H16.0054" />
+      <path d="M11.9955 16H12.0045" />
+      <path d="M7.99451 16H8.00349" />
     </svg>
   );
 };
