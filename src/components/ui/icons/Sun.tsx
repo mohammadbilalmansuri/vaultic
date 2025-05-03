aac2d581628f0ef632f3abfaf357db2e8b762839
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-05-03 14:54:37 +0530

    fix: add stroke property to Moon and Sun icon components for consistency

--- src/components/ui/icons/Sun.tsx
+++ src/components/ui/icons/Sun.tsx
@@ -6,6 +6,7 @@ const Sun = ({ ...props }: SVGProps<SVGSVGElement>) => {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="none"
+      stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
