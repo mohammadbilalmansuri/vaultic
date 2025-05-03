810fc9b11ce877edab405793d4ae5bfe6436a449
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-05-03 15:45:53 +0530

    fix: simplify motion animations in Hero component for improved performance

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,16 +1,14 @@
 "use client";
 import { Button } from "@/components/ui";
-import { motion, useReducedMotion } from "framer-motion";
+import { motion } from "motion/react";
 
 const Hero = () => {
-  const shouldReduceMotion = useReducedMotion();
-
   return (
     <section className="w-full relative flex flex-col sm:flex-row gap-10 items-center py-10">
       <div className="w-full sm:w-[55%] flex flex-col items-start gap-5">
         <motion.h1
           className="text-4xl md:text-5xl font-bold leading-snug heading-color"
-          initial={{ opacity: 0, y: shouldReduceMotion ? 0 : 40 }}
+          initial={{ opacity: 0, y: 40 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.4 }}
         >
@@ -21,7 +19,7 @@ const Hero = () => {
 
         <motion.p
           className="text-base md:text-lg"
-          initial={{ opacity: 0, y: shouldReduceMotion ? 0 : 40 }}
+          initial={{ opacity: 0, y: 40 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.4, delay: 0.2 }}
         >
@@ -31,7 +29,7 @@ const Hero = () => {
 
         <motion.div
           className="flex items-center gap-4 mt-4"
-          initial={{ opacity: 0, y: shouldReduceMotion ? 0 : 40 }}
+          initial={{ opacity: 0, y: 40 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.4, delay: 0.4 }}
         >
@@ -46,8 +44,8 @@ const Hero = () => {
 
       <motion.div
         className="w-full md:w-[45%] aspect-square relative flex items-center justify-center bg-[url(/hero-light.png)] dark:bg-[url(/hero-dark.png)] bg-no-repeat bg-contain bg-right"
-        initial={{ opacity: 0, x: shouldReduceMotion ? 0 : 40 }}
-        animate={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, y: 60 }}
+        animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
       />
     </section>
