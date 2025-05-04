9072035894446221feec10e8ea837dc34d6e4cd1
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-05-04 06:46:29 +0530

    fix: optimize motion animations in Hero component for improved performance

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -4,13 +4,13 @@ import { motion } from "motion/react";
 
 const Hero = () => {
   return (
-    <section className="w-full relative flex flex-col sm:flex-row gap-10 items-center py-10">
+    <section className="w-full relative flex gap-10 items-center">
       <div className="w-full sm:w-[55%] flex flex-col items-start gap-5">
         <motion.h1
           className="text-4xl md:text-5xl font-bold leading-snug heading-color"
-          initial={{ opacity: 0, y: 40 }}
+          initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.4 }}
+          transition={{ duration: 0.3 }}
         >
           Secure by design.
           <br />
@@ -19,9 +19,9 @@ const Hero = () => {
 
         <motion.p
           className="text-base md:text-lg"
-          initial={{ opacity: 0, y: 40 }}
+          initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.4, delay: 0.2 }}
+          transition={{ duration: 0.3, delay: 0.1 }}
         >
           Create and manage Solana and Ethereum wallets with one secure phrase.
           No servers, no tracking — just your crypto, encrypted in your browser.
@@ -29,9 +29,9 @@ const Hero = () => {
 
         <motion.div
           className="flex items-center gap-4 mt-4"
-          initial={{ opacity: 0, y: 40 }}
+          initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.4, delay: 0.4 }}
+          transition={{ duration: 0.3, delay: 0.2 }}
         >
           <Button as="link" href="/onboarding">
             Get Started
@@ -44,9 +44,9 @@ const Hero = () => {
 
       <motion.div
         className="w-full md:w-[45%] aspect-square relative flex items-center justify-center bg-[url(/hero-light.png)] dark:bg-[url(/hero-dark.png)] bg-no-repeat bg-contain bg-right"
-        initial={{ opacity: 0, y: 60 }}
+        initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8 }}
+        transition={{ duration: 0.5 }}
       />
     </section>
   );
