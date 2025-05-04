7084951d7d0c1416a23d5ea55b35dc10b2922d87
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-05-04 06:57:21 +0530

    fix: remove unused colorDependsOnState prop in Switch component for cleaner code

--- src/components/ui/Switch.tsx
+++ src/components/ui/Switch.tsx
@@ -4,29 +4,21 @@ import cn from "@/utils/cn";
 
 interface SwitchProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   state: boolean;
-  colorDependsOnState?: boolean;
 }
 
-const Switch = ({
-  state,
-  colorDependsOnState = true,
-  ...props
-}: SwitchProps) => {
+const Switch = ({ state, ...props }: SwitchProps) => {
   return (
     <button
       type="button"
-      className="p-1 rounded-full h-6 w-12 relative flex items-center transition-colors duration-300 bg-secondary"
+      className="p-1 rounded-full h-6 w-12 relative flex items-center bg-secondary"
       {...props}
     >
       <span
         className={cn(
           "size-4 block rounded-full transition-transform duration-300",
           {
-            "translate-x-6": state,
-            "translate-x-0": !state,
-            "bg-teal-500":
-              !colorDependsOnState || (colorDependsOnState && state),
-            "bg-zinc-500 dark:bg-zinc-400": colorDependsOnState && !state,
+            "translate-x-6 bg-teal-500": state,
+            "translate-x-0 bg-zinc-500 dark:bg-zinc-400": !state,
           }
         )}
       />
