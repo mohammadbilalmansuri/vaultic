58f2058c2cf9d08a8f1e08ac3e625b6278580df0
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-05-04 07:04:56 +0530

    fix: correct border class syntax in NotificationProvider component for consistency

--- src/components/layout/NotificationProvider.tsx
+++ src/components/layout/NotificationProvider.tsx
@@ -25,7 +25,7 @@ const NotificationProvider = () => {
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: -30 }}
           transition={{ duration: 0.2, ease: "easeInOut" }}
-          className="max-w-lg fixed bottom-8 right-8 z-50 backdrop-blur-xl p-5 rounded-2xl flex items-center justify-between gap-4 border-[1.5px] border-color shadow-xl"
+          className="max-w-lg fixed bottom-8 right-8 z-50 backdrop-blur-xl p-5 rounded-2xl flex items-center justify-between gap-4 border-1_5 border-color shadow-xl"
         >
           <div className="flex items-center gap-2.5">
             <span className="min-w-fit">
