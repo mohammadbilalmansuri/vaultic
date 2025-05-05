32adc0b8b153787a6cce8420071ec6acfe34aecf
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-05-05 07:52:01 +0530

    feat: replace WhyVaultic with HowItWorks component and update layout for improved spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { Hero, Features, WhyVaultic } from "@/components/home";
+import { Hero, Features, HowItWorks } from "@/components/home";
 
 const Home = () => {
   return (
-    <div className="w-full max-w-screen-lg relative flex-1 flex flex-col items-center gap-8 py-5">
+    <div className="w-full max-w-screen-lg relative flex-1 flex flex-col items-center gap-32 pt-8 pb-16">
       <Hero />
       <Features />
-      <WhyVaultic />
+      <HowItWorks />
     </div>
   );
 };
