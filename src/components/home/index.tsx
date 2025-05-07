7dcc0962b9ce840d6db034f3b6e8c22ca39d1d5c
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-05-07 07:31:02 +0530

    feat: replace WhyVaultic export with HowItWorks in home component index

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,3 +1,5 @@
 export { default as Hero } from "./Hero";
 export { default as Features } from "./Features";
-export { default as WhyVaultic } from "./WhyVaultic";
+export { default as HowItWorks } from "./HowItWorks";
+export { default as BuildWithConfidence } from "./BuildWithConfidence";
+export { default as FAQ } from "./FAQ";
