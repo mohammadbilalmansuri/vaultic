66e9d0b871ebbec81346db980fd256ca424c6f07
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-05-07 07:37:26 +0530

    fix: adjust visibility threshold for Features component animation

--- src/components/home/Features.tsx
+++ src/components/home/Features.tsx
@@ -33,7 +33,7 @@ const features = [
 
 const Features = () => {
   const ref = useRef(null);
-  const isInView = useInView(ref, { once: true, amount: 0.5 });
+  const isInView = useInView(ref, { once: true, amount: 0.6 });
 
   return (
     <section
