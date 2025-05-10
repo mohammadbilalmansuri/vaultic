4b67f5497e7216523de0b244f30435ddaa33d452
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-05-10 11:36:10 +0530

    rename warning step

--- src/components/onboarding/RecoveryPhraseWarning.tsx
+++ src/components/onboarding/RecoveryPhraseWarning.tsx
@@ -5,43 +5,41 @@ import { Button, Switch } from "@/components/ui";
 import { Warning, Lock } from "@/components/ui/icons";
 import { TOnboardingStep } from "@/types";
 import cn from "@/utils/cn";
+import { scaleUpAnimation } from "@/utils/animations";
 
-type SecretRecoveryWarningProps = {
+type RecoveryPhraseWarningProps = {
   setStep: Dispatch<SetStateAction<TOnboardingStep>>;
 };
 
-const SecretRecoveryWarning = ({ setStep }: SecretRecoveryWarningProps) => {
+const RecoveryPhraseWarning = ({ setStep }: RecoveryPhraseWarningProps) => {
   const [agree, setAgree] = useState(false);
 
   return (
-    <motion.div
-      initial={{ scale: 0.8, opacity: 0 }}
-      animate={{ scale: 1, opacity: 1 }}
-      transition={{ duration: 0.4, ease: "easeInOut" }}
-      className="box max-w-xl"
-    >
-      <h1 className="-mt-1">Secret Recovery Phrase Warning</h1>
+    <motion.div {...scaleUpAnimation()} className="box max-w-xl">
+      <h1 className="-mt-1 onboarding-heading">
+        Secret Recovery Phrase Warning
+      </h1>
       <p className="max-w-sm -mt-1">
         On the next page, you will receive your secret recovery phrase.
       </p>
 
       <div className="mt-1 w-full flex items-center gap-4 text-left p-4 rounded-2xl bg-primary">
         <span className="min-w-fit">
-          <Warning className="w-4.5 fill-yellow-500" />
+          <Warning className="w-6 text-yellow-500" />
         </span>
         <p>
-          This is the<span className="heading-color"> ONLY </span>way to recover
+          This is the <span className="heading-color">ONLY</span> way to recover
           your account if you lose access to your device or password.
         </p>
       </div>
 
       <div className="-mt-1 w-full flex items-center gap-4 text-left p-4 rounded-2xl bg-primary">
         <span className="min-w-fit">
-          <Lock className="w-4.5 fill-teal-500" />
+          <Lock className="w-6 text-teal-500" />
         </span>
         <p>
-          Write it down, store it in a safe place, and
-          <span className="heading-color"> NEVER </span>share it with anyone.
+          Write it down, store it in a safe place, and{" "}
+          <span className="heading-color">NEVER</span> share it with anyone.
         </p>
       </div>
 
@@ -71,4 +69,4 @@ const SecretRecoveryWarning = ({ setStep }: SecretRecoveryWarningProps) => {
   );
 };
 
-export default SecretRecoveryWarning;
+export default RecoveryPhraseWarning;
