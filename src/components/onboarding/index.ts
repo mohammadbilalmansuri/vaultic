25915903380e4321234ff1af69540748a6217646
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-05-10 11:36:22 +0530

    fix: rename SecretRecoveryWarning to RecoveryPhraseWarning for consistency

--- src/components/onboarding/index.ts
+++ src/components/onboarding/index.ts
@@ -1,6 +1,6 @@
 export { default as Welcome } from "./Welcome";
 export { default as SelectNetwork } from "./SelectNetwork";
-export { default as SecretRecoveryWarning } from "./SecretRecoveryWarning";
+export { default as RecoveryPhraseWarning } from "./RecoveryPhraseWarning";
 export { default as GenerateWallet } from "./GenerateWallet";
 export { default as ImportWallet } from "./ImportWallet";
 export { default as CreatePassword } from "./CreatePassword";
