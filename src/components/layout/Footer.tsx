3b1d18bedb537f72d51d6b9dac251737d6477e65
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-05-10 20:16:53 +0530

    fix: update footer copyright text and enhance link styling

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,13 +4,13 @@ const Footer = () => {
   return (
     <footer className="w-full relative flex flex-col items-center px-5 min-h-fit">
       <div className="w-full max-w-screen-lg relative py-5 flex justify-between items-center gap-4">
-        <p>&copy;{new Date().getFullYear()} Vaultic. All rights reserved.</p>
+        <p>&copy;{new Date().getFullYear()} Vaultic</p>
         <p>
           Designed and Developed by{" "}
           <Link
             href="https://www.linkedin.com/in/mohammadbilalmansuri"
             target="_blank"
-            className="heading-color hover:underline"
+            className="heading-color leading-none border-b border-transparent hover:border-current transition-colors duration-300"
           >
             Mohammad Bilal Mansuri
           </Link>
