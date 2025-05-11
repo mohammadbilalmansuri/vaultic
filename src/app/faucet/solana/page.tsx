12af1bde612b2329f966dc98478981d69f27b7e2
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-05-11 15:20:31 +0530

    refactor: update SolanaFaucet component for improved user experience and code clarity

--- src/app/faucet/solana/page.tsx
+++ src/app/faucet/solana/page.tsx
@@ -1,34 +1,31 @@
 "use client";
-import { use, useTransition } from "react";
+import { useTransition } from "react";
 import { motion } from "motion/react";
-import { Button, Input, Loader, CustomInputSelect } from "@/components/ui";
+import { Combobox, Input, Button, Loader } from "@/components/ui";
 import { useBlockchain } from "@/hooks";
 import useNotificationStore from "@/stores/notificationStore";
 import useUserStore from "@/stores/userStore";
+import useWalletStore from "@/stores/walletStore";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
   solanaAirdropSchema,
   TSolanaAirdropFormData,
 } from "@/utils/validations";
-
 import cn from "@/utils/cn";
-import useWalletStore from "@/stores/walletStore";
+import { scaleUpAnimation } from "@/utils/animations";
+import Link from "next/link";
 
 const SolanaFaucet = () => {
   const userExists = useUserStore((state) => state.userExists);
   const wallets = useWalletStore((state) => state.wallets);
   const notify = useNotificationStore((state) => state.notify);
-
   const { airdropSolana } = useBlockchain();
-  const [airdroping, startAirdroping] = useTransition();
+  const [airdropping, startAirdropping] = useTransition();
 
   const {
     register,
     handleSubmit,
-    setError,
-    clearErrors,
-    reset,
     control,
     formState: { errors, isValid },
   } = useForm<TSolanaAirdropFormData>({
@@ -36,93 +33,90 @@ const SolanaFaucet = () => {
     mode: "onChange",
   });
 
-  const handleAirdrop = async ({
-    address,
-    amount,
-  }: {
-    address: string;
-    amount: string;
-  }) => {
-    startAirdroping(async () => {
+  const handleAirdrop = async ({ address, amount }: TSolanaAirdropFormData) => {
+    startAirdropping(async () => {
       try {
         await airdropSolana(address, amount);
+        notify({ type: "success", message: "Airdrop successful!" });
+      } catch (err) {
         notify({
-          type: "success",
-          message: "Airdrop successful!",
+          type: "error",
+          message: err instanceof Error ? err.message : "Airdrop failed!",
         });
-      } catch (error) {
-        const err = error as Error;
-
-        if (
-          err.message.includes("limit") ||
-          err.message.includes("rate limit")
-        ) {
-          notify({
-            type: "error",
-            message: "Airdrop limit reached. Please try after 24 hours.",
-          });
-        } else {
-          notify({
-            type: "error",
-            message: "Airdrop failed. Please try again.",
-          });
-        }
       }
     });
   };
 
   const renderFormError = () => {
     const errorMessage = errors.address?.message || errors.amount?.message;
-
     return errorMessage ? (
       <p className="text-yellow-500">{errorMessage}</p>
     ) : null;
   };
 
+  const solanaWalletOptions = Array.from(wallets.values())
+    .filter(({ network }) => network === "solana")
+    .map(({ index, address }) => ({
+      label: `Solana Wallet ${index + 1}`,
+      value: address,
+    }));
+
   return (
-    <motion.div
-      initial={{ scale: 0.8, opacity: 0 }}
-      animate={{ scale: 1, opacity: 1 }}
-      transition={{ duration: 0.4, ease: "easeInOut" }}
-      className="box max-w-lg"
-    >
-      <h1 className="-mt-1">Solana Devnet Faucet</h1>
-      <p className="max-w-sm">
-        Request SOL tokens for the Solana Devnet. Enter your wallet address and
-        the amount of SOL you want to receive. The maximum amount is 5 SOL and
-        youo can request it once every 24 hours.
-      </p>
+    <motion.div {...scaleUpAnimation()} className="box max-w-lg">
+      <h1 className="box-heading -mt-2">Solana Devnet Faucet</h1>
+      <div className="flex flex-col items-center gap-2">
+        <p>
+          Get free testnet SOL to explore Solana's features and build or test
+          applications.
+        </p>
+        <p>
+          Enter your wallet address and the amount (up to 5 SOL). You can
+          request SOL once every 24 hours for testing, development, or learning
+          purposes.
+        </p>
+      </div>
 
       <form
         onSubmit={handleSubmit(handleAirdrop)}
-        className="w-full flex flex-col gap-4 pt-2"
+        className="w-full flex flex-col gap-4 mt-3"
       >
-        {userExists ? (
-          <CustomInputSelect
+        {userExists && solanaWalletOptions.length > 0 ? (
+          <Combobox
             name="address"
+            placeholder="Enter or select Solana address"
             control={control}
-            options={Array.from(wallets.values())
-              .filter((wallet) => wallet.network === "solana")
-              .map((wallet) => ({
-                label: `Solana Wallet ${wallet.index + 1}`,
-                value: wallet.address,
-              }))}
+            options={solanaWalletOptions}
           />
         ) : (
-          <Input {...register("address")} placeholder="Enter solana address" />
+          <Input {...register("address")} placeholder="Enter Solana address" />
         )}
-        <Input {...register("amount")} placeholder="Enter amount" />
+
+        <Input {...register("amount")} placeholder="Amount (e.g. 1.5)" />
+
         {renderFormError()}
+
         <Button
           type="submit"
-          className={cn("w-full", {
-            "opacity-60 pointer-events-none": !isValid,
+          className={cn("w-full mt-0.5", {
+            "opacity-60 pointer-events-none": !isValid || airdropping,
           })}
-          disabled={!isValid}
+          disabled={!isValid || airdropping}
         >
-          {airdroping ? <Loader size="sm" color="black" /> : "Next"}
+          {airdropping ? <Loader size="sm" color="black" /> : "Request Airdrop"}
         </Button>
       </form>
+
+      <p className="-mb-1 mt-3 flex flex-col gap-2 items-center text-center">
+        <span>Having trouble getting SOL?</span>
+        <Link
+          href="https://faucet.solana.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="transition-all duration-300 border-b border-transparent hover:border-current heading-color leading-tight"
+        >
+          Try the official Solana faucet
+        </Link>
+      </p>
     </motion.div>
   );
 };
