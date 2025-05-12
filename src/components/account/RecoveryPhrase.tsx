dbcbc0ec871e051176e1bfddfc5a8e8bba7009d6
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-05-12 07:04:47 +0530

    fix: update MnemonicView width and improve warning message in RecoveryPhrase component

--- src/components/account/RecoveryPhrase.tsx
+++ src/components/account/RecoveryPhrase.tsx
@@ -4,15 +4,15 @@ import { MnemonicView } from "@/components/common";
 const RecoveryPhrase = () => {
   return (
     <div className="w-full flex flex-col md:flex-row gap-6">
-      <MnemonicView widthClassName="w-[65%]" cols={4} />
+      <MnemonicView widthClassName="w-full md:w-[65%]" cols={4} />
 
-      <div className="w-full md:w-[35%] p-5 rounded-2xl bg-warning text-center flex flex-col justify-center gap-2">
-        <p>
-          You're viewing your recovery phrase. Never share this with anyone.
+      <div className="w-full md:w-[35%] p-5 rounded-2xl bg-warning text-center flex flex-col justify-center gap-3">
+        <p className="font-semibold">
+          Do <span className="underline">not</span> share your Recovery Phrase!
         </p>
         <p>
-          This phrase gives full access to your wallet. Keep it private and
-          secure.
+          If someone has your Recovery Phrase they will have full control of
+          your wallet.
         </p>
       </div>
     </div>
