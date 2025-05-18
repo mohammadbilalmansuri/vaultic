a3525c0ce321ab3831eac5afae7ac4204cf9cce8
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-05-18 22:56:24 +0530

    refactor: add size prop to Switch component for improved customization

--- src/components/ui/Switch.tsx
+++ src/components/ui/Switch.tsx
@@ -3,24 +3,32 @@ import { ButtonHTMLAttributes } from "react";
 import cn from "@/utils/cn";
 
 interface SwitchProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: "sm" | "md";
   state: boolean;
 }
 
-const Switch = ({ state, ...props }: SwitchProps) => {
+const Switch = ({ size = "md", state, ...props }: SwitchProps) => {
   return (
     <button
       type="button"
-      className="p-1 rounded-full h-6 w-12 relative flex items-center bg-secondary"
+      className={cn(
+        "min-w-fit p-1 rounded-full relative flex items-center bg-secondary",
+        {
+          "h-5 w-9": size === "sm",
+          "h-6 w-12": size === "md",
+        }
+      )}
       {...props}
     >
       <span
-        className={cn(
-          "size-4 block rounded-full transition-transform duration-300",
-          {
-            "translate-x-6 bg-teal-500": state,
-            "translate-x-0 bg-zinc-500 dark:bg-zinc-400": !state,
-          }
-        )}
+        className={cn("block rounded-full transition-transform duration-300", {
+          "size-3": size === "sm",
+          "size-4": size === "md",
+          "translate-x-4": size === "sm" && state,
+          "translate-x-6": size === "md" && state,
+          "bg-teal-500": state,
+          "translate-x-0 bg-zinc-500 dark:bg-zinc-400": !state,
+        })}
       />
     </button>
   );
