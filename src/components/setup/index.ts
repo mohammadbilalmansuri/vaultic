c53af981e2faaa6f3b44002992c1b63b4d58e536
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-05-18 17:10:01 +0530

    fix: remove duplicate export of CreatePassword component

--- src/components/setup/index.ts
+++ src/components/setup/index.ts
@@ -1,6 +1,6 @@
 export { default as ChoosePath } from "./ChoosePath";
+export { default as CreatePassword } from "./CreatePassword";
 export { default as RecoveryPhraseWarning } from "./RecoveryPhraseWarning";
 export { default as GenerateWallet } from "./GenerateWallet";
 export { default as ImportWallet } from "./ImportWallet";
-export { default as CreatePassword } from "./CreatePassword";
 export { default as Completion } from "./Completion";
