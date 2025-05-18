4d907d786121c538cbab6fd932e6aad6b260e12b
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-05-18 15:33:16 +0530

    feat: rename welcome to choose path step and improve ux

--- src/components/setup/index.ts
+++ src/components/setup/index.ts
@@ -1,7 +1,7 @@
-export { default as Welcome } from "./Welcome";
-export { default as SelectNetwork } from "./SelectNetwork";
+export { default as ChoosePath } from "./ChoosePath";
 export { default as RecoveryPhraseWarning } from "./RecoveryPhraseWarning";
 export { default as GenerateWallet } from "./GenerateWallet";
 export { default as ImportWallet } from "./ImportWallet";
 export { default as CreatePassword } from "./CreatePassword";
 export { default as Completion } from "./Completion";
+export { default as SetupProgress } from "./SetupProgress";
