e965e6236cef7e4c14798cf442e92fbac50f2f6a
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-05-18 18:50:21 +0530

    refactor: replace RecoveryPhraseWarning and GenerateWallet exports with ShowRecoveryPhrase and ImportRecoveryPhrase

--- src/components/setup/index.ts
+++ src/components/setup/index.ts
@@ -1,6 +1,5 @@
 export { default as ChoosePath } from "./ChoosePath";
 export { default as CreatePassword } from "./CreatePassword";
-export { default as RecoveryPhraseWarning } from "./RecoveryPhraseWarning";
-export { default as GenerateWallet } from "./GenerateWallet";
-export { default as ImportWallet } from "./ImportWallet";
+export { default as ShowRecoveryPhrase } from "./ShowRecoveryPhrase";
+export { default as ImportRecoveryPhrase } from "./ImportRecoveryPhrase";
 export { default as Completion } from "./Completion";
