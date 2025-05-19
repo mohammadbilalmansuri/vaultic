d1c79f32ea78568e76d1e31f31d679f7b1a2e497
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-05-19 13:58:23 +0530

    refactor: reorganize MnemonicView component structure and improve visibility handling

--- src/components/common/MnemonicView.tsx
+++ src/components/common/MnemonicView.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { CopyToggle, EyeToggle } from "@/components/ui";
+import { EyeToggle, CopyToggle } from "@/components/ui";
 import { useClipboard } from "@/hooks";
 import { useWalletStore } from "@/stores";
 import cn from "@/utils/cn";
@@ -12,50 +12,48 @@ interface MnemonicViewProps {
 }
 
 const MnemonicView = ({
-  lable = "Your Recovery Phrase",
   cols = 3,
   widthClassName = "w-full",
 }: MnemonicViewProps) => {
   const mnemonic = useWalletStore((state) => state.mnemonic);
   const copyToClipboard = useClipboard();
+  const [visible, setVisible] = useState(false);
   const [copied, setCopied] = useState(false);
-  const [hidden, setHidden] = useState(true);
 
   return (
     <div
       className={cn(
-        "relative bg-primary rounded-2xl flex flex-col px-2",
+        "relative flex flex-col items-center gap-4",
         widthClassName
       )}
     >
-      <div className="w-full border-color border-b-1.5 flex items-center justify-between py-3 px-2">
-        <p className="leading-none">{lable}</p>
-        <div className="flex items-center gap-4">
-          <EyeToggle
-            hidden={hidden}
-            onClick={() => setHidden((prev) => !prev)}
-          />
-          <CopyToggle
-            copied={copied}
-            onClick={() => copyToClipboard(mnemonic, copied, setCopied)}
-          />
-        </div>
+      <div className="w-full flex items-center justify-between gap-2 px-0.5">
+        <EyeToggle
+          isVisible={visible}
+          onClick={() => setVisible((prev) => !prev)}
+          labels={{ show: "Show Phrase", hide: "Hide Phrase" }}
+        />
+        <CopyToggle
+          hasCopied={copied}
+          onClick={() => copyToClipboard(mnemonic, copied, setCopied)}
+          labels={{ copy: "Copy Phrase", copied: "Copied!" }}
+        />
       </div>
-
       <div
-        className={cn("w-full grid gap-4 cursor-pointer py-4 px-2", {
+        className={cn("w-full grid gap-2 transition-all", {
           "grid-cols-3": cols === 3,
           "grid-cols-4": cols === 4,
           "grid-cols-6": cols === 6,
+          "blur-xs select-none": !visible,
         })}
-        onClick={() => copyToClipboard(mnemonic, copied, setCopied)}
       >
         {mnemonic.split(" ").map((word, index) => (
-          <div key={index} className="flex items-center gap-2">
-            <span className="opacity-80">{index + 1}.</span>
-            <span className="lowercase heading-color">
-              {hidden ? Array(word.length).fill("•").join("") : word}
-            </span>
+          <div
+            key={index}
+            className="flex items-center gap-1 heading-color lowercase w-full p-3.5 h-12 leading-none rounded-2xl bg-white dark:bg-zinc-950/30 border border-color"
+          >
+            <span className="opacity-50">{index + 1}.</span>
+            <span>{word}</span>
           </div>
         ))}
       </div>
