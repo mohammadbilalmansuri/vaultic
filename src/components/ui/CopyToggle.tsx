0012fe9c4402677a809e83b879fd13833d65a20b
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-05-19 11:23:04 +0530

    refactor: add svgProps to CopyToggle for customizable icon rendering

--- src/components/ui/CopyToggle.tsx
+++ src/components/ui/CopyToggle.tsx
@@ -1,20 +1,26 @@
 "use client";
-import { ButtonHTMLAttributes } from "react";
+import { ButtonHTMLAttributes, SVGProps } from "react";
 import { Copy, CopyCheck } from "@/components/ui/icons";
 import cn from "@/utils/cn";
 
 interface CopyProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   copied: boolean;
+  svgProps?: SVGProps<SVGSVGElement>;
 }
 
-const CopyToggle = ({ copied, className = "", ...props }: CopyProps) => {
+const CopyToggle = ({
+  copied,
+  className = "",
+  svgProps,
+  ...props
+}: CopyProps) => {
   return (
     <button
       type="button"
       className={cn("icon-btn", { "pointer-events-none": copied }, className)}
       {...props}
     >
-      {copied ? <CopyCheck /> : <Copy />}
+      {copied ? <CopyCheck {...svgProps} /> : <Copy {...svgProps} />}
     </button>
   );
 };
