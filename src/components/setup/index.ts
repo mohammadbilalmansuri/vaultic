2abd99d2ff7095f5084a0f0357ca83c7df50b718
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-05-20 23:19:42 +0530

    refactor: replace Completion component with SetupComplete and update references

--- src/components/setup/index.ts
+++ src/components/setup/index.ts
@@ -3,4 +3,4 @@ export { default as CreatePassword } from "./CreatePassword";
 export { default as ShowRecoveryPhrase } from "./ShowRecoveryPhrase";
 export { default as EnterRecoveryPhrase } from "./EnterRecoveryPhrase";
 export { default as SettingUpWallet } from "./SettingUpWallet";
-export { default as Completion } from "./Completion";
+export { default as SetupComplete } from "./SetupComplete";
