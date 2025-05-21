92a08461a585df0d27c9ed19e26684a80cc501c6
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-05-21 11:31:28 +0530

    fix: update header navigation to conditionally render links based on wallet existence

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,14 +3,19 @@ import { ThemeSwitcher, NavLink } from "@/components/ui";
 import { Logo, Github } from "@/components/ui/icons";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { useWalletStore } from "@/stores";
 
 const Header = () => {
   const pathname = usePathname();
+  const walletExists = useWalletStore((state) => state.walletExists);
 
   return (
     <header className="w-full relative flex flex-col items-center px-5 min-h-fit">
       <div className="w-full max-w-screen-lg relative flex items-center justify-between gap-5 py-5">
-        <Link href="/" className="flex items-center gap-2.5 select-none">
+        <Link
+          href={walletExists ? "/dashboard" : "/"}
+          className="flex items-center gap-2.5 select-none"
+        >
           <Logo className="w-7 text-teal-500" />
           <span className="-mt-0.5 text-3xl lowercase leading-[0.8] font-bold text-teal-500">
             vaultic
@@ -18,9 +23,15 @@ const Header = () => {
         </Link>
 
         <nav className="flex items-center gap-6">
-          <NavLink href="/setup" active={pathname === "/setup"}>
-            Set Up Your Wallet
-          </NavLink>
+          {walletExists ? (
+            <NavLink href="/dashboard" active={pathname === "/dashboard"}>
+              Dashboard
+            </NavLink>
+          ) : (
+            <NavLink href="/setup" active={pathname === "/setup"}>
+              Set Up Your Wallet
+            </NavLink>
+          )}
           <NavLink href="/faucet" active={pathname === "/faucet"}>
             Faucet
           </NavLink>
