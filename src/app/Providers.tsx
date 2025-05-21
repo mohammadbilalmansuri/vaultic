9d60182d9e23c5f5099333b76e0566a25cc59beb
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-05-21 13:35:17 +0530

    feat: add WalletChecker component to manage wallet state and enhance provider structure

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,11 +1,15 @@
 "use client";
 import { LayoutProps } from "@/types";
-import { ThemeProvider, NotificationProvider } from "@/components/layout";
+import {
+  ThemeProvider,
+  NotificationProvider,
+  WalletChecker,
+} from "@/components/layout";
 
 const Providers = ({ children }: LayoutProps) => {
   return (
     <ThemeProvider>
-      {children}
+      <WalletChecker>{children}</WalletChecker>
       <NotificationProvider />
     </ThemeProvider>
   );
