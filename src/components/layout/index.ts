052ff2f1649fb42434d86e60810117918b678ced
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-05-21 13:53:23 +0530

    feat: add PublicLayout component for consistent page structure

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,7 +1,6 @@
-export { default as Footer } from "./Footer";
-export { default as Header } from "./Header";
-export { default as NotificationProvider } from "./NotificationProvider";
 export { default as ThemeProvider } from "./ThemeProvider";
+export { default as NotificationProvider } from "./NotificationProvider";
 export { default as WalletChecker } from "./WalletChecker";
+export { default as PublicLayout } from "./PublicLayout";
 export { default as Protected } from "./Protected";
 export { default as TestnetNotice } from "./TestnetNotice";
