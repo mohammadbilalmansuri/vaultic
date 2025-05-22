d93863f98ac6dcea67a36aafb277ff48298f5200
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-05-22 17:37:25 +0530

    refactor: update WalletChecker component to use IChildren type for props

--- src/components/layout/WalletChecker.tsx
+++ src/components/layout/WalletChecker.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { useEffect } from "react";
-import { LayoutProps } from "@/types";
+import { IChildren } from "@/types";
 import useWalletStore from "@/stores/walletStore";
 import { useWallet } from "@/hooks";
 
-const WalletChecker = ({ children }: LayoutProps) => {
+const WalletChecker = ({ children }: IChildren) => {
   const { checkWalletExists } = useWallet();
   const walletStatus = useWalletStore((state) => state.walletStatus);
 
