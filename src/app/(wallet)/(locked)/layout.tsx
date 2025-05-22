ac88e132b9791b513ad68f14248a37fcb89594be
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-05-22 17:42:08 +0530

    refactor: simplify LockedLayout by removing unused code and implementing UnlockGuard

--- src/app/(wallet)/(locked)/layout.tsx
+++ src/app/(wallet)/(locked)/layout.tsx
@@ -1,22 +1,13 @@
-"use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { useWalletStore } from "@/stores";
-import { LayoutProps } from "@/types";
+import { IChildren } from "@/types";
+import { UnlockGuard, Sidebar } from "@/components/layout";
+import { DashboardShell } from "@/components/shells";
 
-const LockedLayout = ({ children }: LayoutProps) => {
-  const router = useRouter();
-  const { authenticated, walletStatus } = useWalletStore();
-
-  useEffect(() => {
-    if (walletStatus === "ready" && !authenticated) {
-      router.replace("/unlock");
-    }
-  }, []);
-
-  if (walletStatus === "checking") return null;
-
-  return children;
+const LockedLayout = ({ children }: IChildren) => {
+  return (
+    <UnlockGuard>
+      <DashboardShell>{children}</DashboardShell>
+    </UnlockGuard>
+  );
 };
 
 export default LockedLayout;
