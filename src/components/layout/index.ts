68a94727e39f2f02835a8d186c45a54be9151521
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-05-22 17:40:00 +0530

    refactor: remove PublicLayout export and add Header and Footer exports

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,7 +1,8 @@
 export { default as ThemeProvider } from "./ThemeProvider";
 export { default as NotificationProvider } from "./NotificationProvider";
 export { default as WalletChecker } from "./WalletChecker";
-export { default as PublicLayout } from "./PublicLayout";
 export { default as UnlockGuard } from "./UnlockGuard";
+export { default as Header } from "./Header";
+export { default as Footer } from "./Footer";
 export { default as Sidebar } from "./Sidebar";
 export { default as TestnetNotice } from "./TestnetNotice";
