373090c7165600dbc6d8020c2eee7aa71eabe39d
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-05-22 16:35:24 +0530

    feat: update layout exports to include UnlockGuard and Sidebar components

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -2,5 +2,6 @@ export { default as ThemeProvider } from "./ThemeProvider";
 export { default as NotificationProvider } from "./NotificationProvider";
 export { default as WalletChecker } from "./WalletChecker";
 export { default as PublicLayout } from "./PublicLayout";
-export { default as Protected } from "./Protected";
+export { default as UnlockGuard } from "./UnlockGuard";
+export { default as Sidebar } from "./Sidebar";
 export { default as TestnetNotice } from "./TestnetNotice";
