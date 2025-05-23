8d38dfe8729c60fed10f8b749af2f3134e1c1d55
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-05-23 16:05:56 +0530

    refactor: change UnlockForm structure to separate form and motion components

--- src/components/layout/UnlockForm.tsx
+++ src/components/layout/UnlockForm.tsx
@@ -31,31 +31,34 @@ const UnlockForm = () => {
   };
 
   return (
-    <motion.form
-      {...scaleUpAnimation()}
-      className="box p-12"
-      onSubmit={handleSubmit(handleUnlock)}
-    >
+    <motion.div {...scaleUpAnimation()} className="box p-12">
       <Logo className="w-15 text-teal-500" />
-      <h2 className="mt-3 mb-1">Enter Your Password</h2>
-      <PasswordInput {...register("password")} />
-      <Button
-        type="submit"
-        className={cn("w-full", {
-          "opacity-60 pointer-events-none": !isValid,
-        })}
-        disabled={!isValid || unlocking}
+      <h2 className="mt-3">Enter Your Password</h2>
+
+      <form
+        onSubmit={handleSubmit(handleUnlock)}
+        className="w-full flex flex-col gap-4 mt-1"
       >
-        {unlocking ? <Loader size="sm" color="black" /> : "Unlock"}
-      </Button>
-      <FormError errors={errors} className="mt-1.5 -mb-2" />
+        <PasswordInput {...register("password")} />
+        <Button
+          type="submit"
+          className={cn("w-full", {
+            "opacity-60 pointer-events-none": !isValid,
+          })}
+          disabled={!isValid || unlocking}
+        >
+          {unlocking ? <Loader size="sm" color="black" /> : "Unlock"}
+        </Button>
+        <FormError errors={errors} className="mt-1.5 -mb-2" />
+      </form>
+
       <Link
         href="/forgot-password"
         className="mt-2 leading-snug border-b border-current hover:heading-color transition-colors duration-300"
       >
         Forgot Password
       </Link>
-    </motion.form>
+    </motion.div>
   );
 };
 
