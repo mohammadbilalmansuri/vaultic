04e5490fe56ed203a925e18a49d8ab2f9abc00b6
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-05-23 10:56:25 +0530

    refactor: remove UnlockGuard export and replace with UnlockForm in layout index

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,7 +1,7 @@
 export { default as ThemeProvider } from "./ThemeProvider";
 export { default as NotificationProvider } from "./NotificationProvider";
 export { default as WalletChecker } from "./WalletChecker";
-export { default as UnlockGuard } from "./UnlockGuard";
+export { default as UnlockForm } from "./UnlockForm";
 export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
 export { default as Sidebar } from "./Sidebar";
