57703d1da9faad1505e22088a05173363d2f6644
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-05-25 12:24:43 +0530

    refactor: adjust layout properties in DashboardShell for improved responsiveness

--- src/components/shells/DashboardShell.tsx
+++ src/components/shells/DashboardShell.tsx
@@ -3,11 +3,11 @@ import { Sidebar, TestnetNotice } from "../layout";
 
 const DashboardShell = ({ children }: IChildren) => {
   return (
-    <div className="w-full relative flex-1 flex overflow-hidden">
+    <div className="size-full relative max-h-dvh flex overflow-hidden">
       <Sidebar />
       <main className="w-full relative flex flex-col">
         <TestnetNotice />
-        <div className="w-full relative flex flex-col items-center p-8">
+        <div className="w-full relative flex-1 flex flex-col items-center p-8 overflow-y-auto scrollbar-thin">
           {children}
         </div>
       </main>
