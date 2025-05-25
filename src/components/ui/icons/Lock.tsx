8ca3defa983418992c12565b047e4102f5a4695a
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-05-25 11:18:11 +0530

    fix: update Lock icon SVG properties for improved styling and consistency

--- src/components/ui/icons/Lock.tsx
+++ src/components/ui/icons/Lock.tsx
@@ -7,19 +7,13 @@ const Lock = ({ ...props }: SVGProps<SVGSVGElement>) => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth="1.5"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...props}
     >
-      <path d="M2 16C2 13.1716 2 11.7574 2.87868 10.8787C3.75736 10 5.17157 10 8 10H16C18.8284 10 20.2426 10 21.1213 10.8787C22 11.7574 22 13.1716 22 16C22 18.8284 22 20.2426 21.1213 21.1213C20.2426 22 18.8284 22 16 22H8C5.17157 22 3.75736 22 2.87868 21.1213C2 20.2426 2 18.8284 2 16Z" />
-      <path
-        d="M6 10V8C6 4.68629 8.68629 2 12 2C15.3137 2 18 4.68629 18 8V10"
-        strokeLinecap="round"
-      />
-      <path
-        d="M8 16H8.009M11.991 16H12M15.991 16H16"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
+      <rect width="18" height="11" x="3" y="11" rx="2" ry="2" />
+      <path d="M7 11V7a5 5 0 0 1 10 0v4" />
     </svg>
   );
 };
