aafd74d3e771e8521a1bd351c74a22f6210d13c2
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-05-25 11:17:03 +0530

    fix: change main tag to div in HomePage for consistency in layout structure

--- src/app/(public)/(guest)/page.tsx
+++ src/app/(public)/(guest)/page.tsx
@@ -9,13 +9,13 @@ import {
 
 const HomePage = () => {
   return (
-    <main className="w-full max-w-screen-lg relative flex-1 flex flex-col items-center gap-32 pt-8 pb-16">
+    <div className="w-full max-w-screen-lg relative flex-1 flex flex-col items-center gap-32 pt-8 pb-16">
       <HeroSection />
       <FeaturesSection />
       <HowItWorksSection />
       <TestnetSection />
       <FAQSection />
-    </main>
+    </div>
   );
 };
 
