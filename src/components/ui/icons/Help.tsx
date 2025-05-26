d0a4881abaa3e97e54154672c2accc5789395088
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-05-26 22:09:18 +0530

    feat: add Help, Home, and WiFiOff icons components

---
+++ src/components/ui/icons/Help.tsx
@@ -0,0 +1,22 @@
+import { SVGProps } from "react";
+
+const Help = ({ ...props }: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <circle cx="12" cy="12" r="10" />
+      <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
+      <path d="M12 17h.01" />
+    </svg>
+  );
+};
+
+export default Help;
