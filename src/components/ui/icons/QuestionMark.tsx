39bb88f0bdc49d2b418e5aa948673504ea4a5095
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-05-28 08:11:34 +0530

    rename help icon to questionmark

--- src/components/ui/icons/QuestionMark.tsx
+++ src/components/ui/icons/QuestionMark.tsx
@@ -1,6 +1,6 @@
 import { SVGProps } from "react";
 
-const Help = ({ ...props }: SVGProps<SVGSVGElement>) => {
+const QuestionMark = ({ ...props }: SVGProps<SVGSVGElement>) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -19,4 +19,4 @@ const Help = ({ ...props }: SVGProps<SVGSVGElement>) => {
   );
 };
 
-export default Help;
+export default QuestionMark;
