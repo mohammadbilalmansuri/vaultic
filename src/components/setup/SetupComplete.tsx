84fd8fa8bbac06f37fa399beb3e67c0aa5eecc6c
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-05-28 08:12:51 +0530

    refactor: remove unnecessary line breaks in SetupComplete component

--- src/components/setup/SetupComplete.tsx
+++ src/components/setup/SetupComplete.tsx
@@ -12,15 +12,12 @@ const SetupComplete = ({ path }: { path: TSetupPath }) => {
       className="box p-12 text-center flex flex-col items-center"
     >
       <Logo className="w-15 text-teal-500" />
-
       <h2 className="mt-3">Your wallet is ready</h2>
-
       <p>
         You’ve successfully {path === "create" ? "created" : "imported"} your
         wallet. You’re now ready to explore, send, and manage your assets
         securely.
       </p>
-
       <Button as="link" href="/dashboard" className="mt-3">
         Enter Dashboard
       </Button>
