8cfd0b56a3ecafdc8881e4feca7be903b7242f52
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-05-29 08:49:43 +0530

    feat: enhance getShortAddress function to include network parameter for address formatting

--- src/utils/getShortAddress.ts
+++ src/utils/getShortAddress.ts
@@ -1,6 +1,15 @@
-const getShortAddress = (address: string, chars = 4): string => {
-  if (!address || address.length <= chars * 2) return address;
-  return `${address.slice(0, chars)}...${address.slice(-chars)}`;
+import { TNetwork } from "@/types";
+
+const getShortAddress = (
+  address: string,
+  network: TNetwork,
+  chars = 4
+): string => {
+  if (!address || address.length <= chars * 2 + 2) return address;
+  return `${address.slice(
+    0,
+    network === "ethereum" ? chars + 2 : chars
+  )}...${address.slice(-chars)}`;
 };
 
 export default getShortAddress;
