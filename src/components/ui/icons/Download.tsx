85192ae61924c72262e8ce9bcadf93df8f7594af
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-05-29 06:14:58 +0530

    feat: add Camera, Download, Refresh, and Upload icon components

---
+++ src/components/ui/icons/Download.tsx
@@ -0,0 +1,22 @@
+import { SVGProps } from "react";
+
+const Download = ({ ...props }: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M12 15V3" />
+      <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+      <path d="m7 10 5 5 5-5" />
+    </svg>
+  );
+};
+
+export default Download;
