10e400e96cb0b1834743a9435964f2c148b6a2b5
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-05-31 07:56:25 +0530

    refactor: adjust height and width properties in DashboardShell for layout consistency

--- src/components/shells/DashboardShell.tsx
+++ src/components/shells/DashboardShell.tsx
@@ -3,9 +3,9 @@ import { Sidebar, TestnetNotice } from "../layout";
 
 const DashboardShell = ({ children }: IChildren) => {
   return (
-    <div className="w-full max-h-dvh relative flex-1 flex overflow-hidden">
+    <div className="w-full h-dvh relative flex-1 flex overflow-hidden">
       <Sidebar />
-      <main className="w-full relative flex flex-col">
+      <main className="w-[calc(100%-18rem)] relative flex flex-col">
         <TestnetNotice />
         <div className="w-full relative flex-1 flex flex-col items-center p-8 overflow-y-auto scrollbar-thin">
           {children}
