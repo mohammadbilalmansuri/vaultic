29bd901e9d45c6e626ac4d84d396fd3aaa8a633d
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-06-03 06:22:59 +0530

    feat: add useActivityStore to the stores index

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,3 +2,4 @@ export { default as useThemeStore } from "./themeStore";
 export { default as useNotificationStore } from "./notificationStore";
 export { default as useWalletStore } from "./walletStore";
 export { default as useAccountsStore } from "./accountsStore";
+export { default as useActivityStore } from "./activityStore";
