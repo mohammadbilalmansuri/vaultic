65640d6cd0f1d28176baecbab243b3cf8407881a
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-06-10 07:34:24 +0530

    refactor: simplify step handling in SetupPage component for improved readability

--- src/app/(public)/(guest)/setup/page.tsx
+++ src/app/(public)/(guest)/setup/page.tsx
@@ -1,6 +1,8 @@
 "use client";
-import { useState, JSX } from "react";
-import { TSetupPath, TSetupStep, TSetupSetPath, TSetupSetStep } from "@/types";
+import { useState } from "react";
+import { AnimatePresence } from "motion/react";
+import { TSetupPath, TSetupStep } from "@/types";
+import { StepProgress } from "@/components/ui";
 import {
   ChoosePath,
   CreatePassword,
@@ -9,40 +11,10 @@ import {
   SettingUpWallet,
   SetupComplete,
 } from "@/components/setup";
-import { Loader } from "@/components/ui";
 
-interface IGetStepComponent {
-  step: TSetupStep;
-  path: TSetupPath;
-  setStep: TSetupSetStep;
-  setPath: TSetupSetPath;
-}
-
-const getStepComponent = ({
-  step,
-  path,
-  setStep,
-  setPath,
-}: IGetStepComponent): JSX.Element => {
-  switch (step) {
-    case 1:
-      return <ChoosePath setPath={setPath} setStep={setStep} />;
-    case 2:
-      return <CreatePassword setStep={setStep} />;
-    case 3:
-      return path === "create" ? (
-        <ShowRecoveryPhrase setStep={setStep} />
-      ) : (
-        <EnterRecoveryPhrase setStep={setStep} />
-      );
-    case 4:
-      return <SettingUpWallet path={path} setStep={setStep} />;
-    case 5:
-      return <SetupComplete path={path} />;
-    default:
-      return <Loader />;
-  }
-};
+const getStepProgress = (activeDot: number, backFn?: () => void) => (
+  <StepProgress dots={3} activeDot={activeDot} back={backFn} />
+);
 
 const SetupPage = () => {
   const [step, setStep] = useState<TSetupStep>(1);
@@ -50,7 +22,45 @@ const SetupPage = () => {
 
   return (
     <div className="w-full max-w-screen-lg relative flex flex-col items-center">
-      {getStepComponent({ step, path, setStep, setPath })}
+      <AnimatePresence mode="wait">
+        {step === 1 && (
+          <ChoosePath setPath={setPath} setStep={setStep} key="choose-path" />
+        )}
+
+        {step === 2 && (
+          <CreatePassword
+            setStep={setStep}
+            key="create-password"
+            StepProgress={getStepProgress(1, () => setStep(1))}
+          />
+        )}
+
+        {step === 3 &&
+          (path === "create" ? (
+            <ShowRecoveryPhrase
+              setStep={setStep}
+              key="show-recovery-phrase"
+              StepProgress={getStepProgress(2)}
+            />
+          ) : (
+            <EnterRecoveryPhrase
+              setStep={setStep}
+              key="enter-recovery-phrase"
+              StepProgress={getStepProgress(2, () => setStep(2))}
+            />
+          ))}
+
+        {step === 4 && (
+          <SettingUpWallet
+            path={path}
+            setStep={setStep}
+            key="setting-up-wallet"
+            StepProgress={getStepProgress(3)}
+          />
+        )}
+
+        {step === 5 && <SetupComplete path={path} key="setup-complete" />}
+      </AnimatePresence>
     </div>
   );
 };
