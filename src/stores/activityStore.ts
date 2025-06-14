62b56b5aa22b0616a45045a350861df55e59dc4d
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-06-14 10:18:01 +0530

    fix: add addActivity method to IActivityStore for activity management

--- src/stores/activityStore.ts
+++ src/stores/activityStore.ts
@@ -5,12 +5,15 @@ interface IActivityStore {
   activities: IActivity[];
   clearActivities: () => void;
   setActivities: (activities: IActivity[]) => void;
+  addActivity: (activity: IActivity) => void;
 }
 
 const useActivityStore = create<IActivityStore>((set, get) => ({
   activities: [],
   clearActivities: () => set({ activities: [] }),
   setActivities: (activities: IActivity[]) => set({ activities }),
+  addActivity: (activity: IActivity) =>
+    set((state) => ({ activities: [...state.activities, activity] })),
 }));
 
 export default useActivityStore;
