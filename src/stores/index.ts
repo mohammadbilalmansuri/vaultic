8f329ed30253b5fd6b4055551105e7a8ce1f47af
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-06-20 12:46:23 +0530

    fix: reorder store exports and rename activityStore to transactionStore

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,5 +1,5 @@
-export { default as useThemeStore } from "./themeStore";
-export { default as useNotificationStore } from "./notificationStore";
-export { default as useWalletStore } from "./walletStore";
 export { default as useAccountsStore } from "./accountsStore";
-export { default as useActivityStore } from "./activityStore";
+export { default as useNotificationStore } from "./notificationStore";
+export { default as useThemeStore } from "./themeStore";
+export { default as useTransactionsStore } from "./transactionsStore";
+export { default as useWalletStore } from "./walletStore";
