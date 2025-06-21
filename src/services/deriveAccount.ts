eb978087bf901d003e036b873969fad9ab07336c
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-06-21 18:59:21 +0530

    fix: refactor deriveAccount function to utilize NETWORKS for dynamic account derivation

--- src/services/deriveAccount.ts
+++ src/services/deriveAccount.ts
@@ -1,7 +1,6 @@
 import { mnemonicToSeed } from "bip39";
+import { NETWORKS } from "@/constants";
 import { TAccount } from "@/types";
-import { deriveEthereumAccount } from "./ethereum";
-import { deriveSolanaAccount } from "./solana";
 
 const deriveAccount = async (
   mnemonic: string,
@@ -9,12 +8,16 @@ const deriveAccount = async (
 ): Promise<TAccount> => {
   const seed = await mnemonicToSeed(mnemonic);
 
-  const [ethereum, solana] = await Promise.all([
-    deriveEthereumAccount(seed, index),
-    deriveSolanaAccount(seed, index),
-  ]);
+  const accountEntries = await Promise.all(
+    Object.values(NETWORKS).map(
+      async ({ id, functions: { deriveNetworkAccount } }) => {
+        const { address, privateKey } = await deriveNetworkAccount(seed, index);
+        return [id, { address, privateKey, balance: "0" }] as const;
+      }
+    )
+  );
 
-  return { ethereum, solana };
+  return Object.fromEntries(accountEntries) as TAccount;
 };
 
 export default deriveAccount;
