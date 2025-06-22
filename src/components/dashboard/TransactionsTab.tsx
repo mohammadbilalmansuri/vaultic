db66b789cd65cf0837b708eb4a46b2e3e5075dbb
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-06-22 12:39:14 +0530

    refactor: replace ActivityTab with TransactionsTab in dashboard components

---
+++ src/components/dashboard/TransactionsTab.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import { useTransactionsStore } from "@/stores";
+
+const TransactionsTab = () => {
+  return <div>TransactionsTab</div>;
+};
+
+export default TransactionsTab;
