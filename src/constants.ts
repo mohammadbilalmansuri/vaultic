809f378470f5d73ad82a9b9f68aeeb66b2d3d7bf
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-06-22 08:26:33 +0530

    refactor: reorganize DEFAULT_NETWORK declaration in constants file

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,7 @@
+import { TNetwork } from "./types";
+
+export const DEFAULT_NETWORK: TNetwork = "ethereum";
+
 export const BALANCE_DISPLAY_DECIMALS = 4;
 
 export const FAUCET_PRESET_AMOUNTS = ["0.5", "1", "2.5", "5"];
