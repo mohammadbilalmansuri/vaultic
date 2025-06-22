f98bf40c750e3c36f0c8a267b57ab1234ebd8c4b
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-06-22 08:04:53 +0530

    feat: add NETWORKS configuration

---
+++ src/config/networks.ts
@@ -0,0 +1,34 @@
+import { Ethereum, Solana } from "../components/ui/icons";
+
+export const NETWORKS = {
+  ethereum: {
+    id: "ethereum",
+    name: "Ethereum",
+    token: "ETH",
+    icon: Ethereum,
+    testnetName: "Sepolia",
+    decimals: 18,
+    fee: "0.0001",
+    rentExemption: "0",
+    svgUrlForQR: "/ethereum.svg",
+    rpc: {
+      mainnet: process.env.NEXT_PUBLIC_ETH_MAINNET_RPC!,
+      testnet: process.env.NEXT_PUBLIC_ETH_SEPOLIA_RPC!,
+    },
+  },
+  solana: {
+    id: "solana",
+    name: "Solana",
+    token: "SOL",
+    icon: Solana,
+    testnetName: "Devnet",
+    decimals: 9,
+    fee: "0.00008",
+    rentExemption: "0.00089088",
+    svgUrlForQR: "/solana.svg",
+    rpc: {
+      mainnet: process.env.NEXT_PUBLIC_SOLANA_MAINNET_RPC!,
+      testnet: process.env.NEXT_PUBLIC_SOLANA_DEVNET_RPC!,
+    },
+  },
+};
