8cb31aca52b73c6646232abdfd8ae8f216d7aab3
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-06-30 07:16:09 +0530

    refactor: remove NetworkCard component and update index export

--- src/components/dashboard/index.ts
+++ src/components/dashboard/index.ts
@@ -1,4 +1,3 @@
-export { default as NetworkCard } from "./NetworkCard";
 export { default as SendTab } from "./SendTab";
 export { default as ReceiveTab } from "./ReceiveTab";
 export { default as TransactionsTab } from "./TransactionsTab";
