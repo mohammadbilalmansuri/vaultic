e9b5852dac652d1c07d6f03a26835fb151740770
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-06-30 10:55:28 +0530

    feat: enhance SettingsPage with animation and wallet lock functionality

--- src/app/(wallet)/(locked)/settings/page.tsx
+++ src/app/(wallet)/(locked)/settings/page.tsx
@@ -1,12 +1,16 @@
 "use client";
+import { motion } from "motion/react";
 import { TTabs } from "@/types";
+import { fadeUpAnimation } from "@/utils/animations";
+import { useWallet } from "@/hooks";
 import {
   ChangePassword,
   TestnetMode,
   RemoveAccount,
   RecoveryPhrase,
 } from "@/components/settings";
-import { Tabs } from "@/components/ui";
+import { Tabs, Tooltip } from "@/components/ui";
+import { Lock, Wallet } from "@/components/ui/icons";
 
 const TABS: TTabs = {
   "Recovery Phrase": { content: RecoveryPhrase },
@@ -16,8 +20,31 @@ const TABS: TTabs = {
 } as const;
 
 const SettingsPage = () => {
+  const { lockWallet } = useWallet();
+
   return (
-    <div className="w-full max-w-screen-lg relative flex flex-col flex-1 py-5">
+    <div className="w-full max-w-screen-lg relative flex flex-col gap-6 flex-1">
+      <motion.div
+        className="w-full relative flex items-center justify-between gap-4"
+        {...fadeUpAnimation()}
+      >
+        <div className="flex items-center gap-4">
+          <div className="size-12 rounded-2xl bg-teal-500/10 flex items-center justify-center">
+            <Wallet className="w-6 text-teal-500" />
+          </div>
+          <div className="flex flex-col">
+            <h2 className="text-xl font-semibold heading-color">Settings</h2>
+            <p>Manage your wallet settings and preferences.</p>
+          </div>
+        </div>
+
+        <Tooltip content="Lock Wallet">
+          <button className="icon-btn-bg" onClick={lockWallet}>
+            <Lock />
+          </button>
+        </Tooltip>
+      </motion.div>
+
       <Tabs tabs={TABS} />
     </div>
   );
