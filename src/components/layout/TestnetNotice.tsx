64211b63694ed5930bed10e10e22754eeb2534c7
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-01 21:44:47 +0530

    fix: remove authentication check from TestnetNotice component

--- src/components/layout/TestnetNotice.tsx
+++ src/components/layout/TestnetNotice.tsx
@@ -3,9 +3,8 @@ import { useWalletStore } from "@/stores";
 
 const TestnetNotice = () => {
   const networkMode = useWalletStore((state) => state.networkMode);
-  const authenticated = useWalletStore((state) => state.authenticated);
 
-  if (!authenticated || networkMode !== "testnet") return null;
+  if (networkMode !== "testnet") return null;
 
   return (
     <p className="w-full bg-warning px-5 py-2 flex items-center justify-center">
