96e512cbfa66b3193768e853c900e59008e77a98
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-01 07:30:10 +0530

    feat: enhance RecoveryPhrase component with animation and props

--- src/components/settings/RecoveryPhrase.tsx
+++ src/components/settings/RecoveryPhrase.tsx
@@ -1,28 +1,41 @@
 "use client";
+import { motion } from "motion/react";
+import { ITabContentProps } from "@/types";
 import { useWalletStore } from "@/stores";
+import { fadeUpAnimation } from "@/utils/animations";
 import { MnemonicView } from "../ui";
 
-const RecoveryPhrase = () => {
+const RecoveryPhrase = ({
+  showInitialAnimation,
+  initialAnimationDelay,
+}: ITabContentProps) => {
   const mnemonic = useWalletStore((state) => state.mnemonic);
 
-  return (
-    <div className="w-full flex flex-col md:flex-row gap-6">
-      <MnemonicView
-        mnemonic={mnemonic}
-        widthClassName="w-full md:w-[65%]"
-        cols={4}
-      />
+  const animationProps = fadeUpAnimation({
+    delay: showInitialAnimation ? initialAnimationDelay : undefined,
+  });
 
-      <div className="w-full md:w-[35%] p-5 rounded-2xl bg-warning text-center flex flex-col justify-center gap-3">
-        <p className="font-semibold">
-          Do <span className="underline">not</span> share your Recovery Phrase!
-        </p>
-        <p>
-          If someone has your Recovery Phrase they will have full control of
-          your wallet.
-        </p>
+  return (
+    <motion.div className="box max-w-lg gap-0" {...animationProps}>
+      <h3 className="w-full text-lg font-medium heading-color border-b-1.5 border-color py-3">
+        Your Recovery Phrase
+      </h3>
+
+      <div className="w-full flex flex-col gap-6 p-6">
+        <div className="w-full p-5 rounded-2xl bg-warning text-center flex flex-col justify-center gap-2.5">
+          <p className="font-semibold">
+            Do <span className="underline">not</span> share your Recovery
+            Phrase!
+          </p>
+          <p>
+            If someone has your Recovery Phrase they will have full control of
+            your wallet.
+          </p>
+        </div>
+
+        <MnemonicView mnemonic={mnemonic} />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
