d7dbdd76d4a8f099df3dea6bad1d0df26bf945e0
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-07-01 15:50:19 +0530

    fix: refactor Switch component structure for improved layout

--- src/components/ui/Switch.tsx
+++ src/components/ui/Switch.tsx
@@ -14,26 +14,31 @@ const Switch = ({
   ...props
 }: SwitchProps) => {
   return (
-    <button
-      type="button"
-      className={cn(
-        "min-w-fit p-1 rounded-full relative flex items-center bg-secondary",
-        { "h-5 w-9": size === "sm", "h-6 w-12": size === "md" },
-        className
-      )}
-      {...props}
-    >
-      <span
-        className={cn("block rounded-full transition-transform duration-300", {
-          "size-3": size === "sm",
-          "size-4": size === "md",
-          "translate-x-4": size === "sm" && state,
-          "translate-x-6": size === "md" && state,
-          "bg-teal-500": state,
-          "translate-x-0 bg-zinc-500 dark:bg-zinc-400": !state,
-        })}
-      />
-    </button>
+    <div className="min-w-fit relative">
+      <button
+        type="button"
+        className={cn(
+          "p-1 rounded-full flex items-center bg-secondary",
+          { "h-5 w-9": size === "sm", "h-6 w-12": size === "md" },
+          className
+        )}
+        {...props}
+      >
+        <span
+          className={cn(
+            "block rounded-full transition-transform duration-300",
+            {
+              "size-3": size === "sm",
+              "size-4": size === "md",
+              "translate-x-4": size === "sm" && state,
+              "translate-x-6": size === "md" && state,
+              "bg-teal-500": state,
+              "translate-x-0 bg-zinc-500 dark:bg-zinc-400": !state,
+            }
+          )}
+        />
+      </button>
+    </div>
   );
 };
 
