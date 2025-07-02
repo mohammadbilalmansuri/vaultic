e6a517c39e05155706b775b48fb5b5e05bbf9158
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-02 08:09:56 +0530

    fix: update footer content for clarity and improved structure

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,13 +3,15 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="w-full relative flex flex-col items-center px-5 min-h-fit">
-      <div className="w-full max-w-screen-lg relative py-5 flex justify-between items-center gap-4">
-        <p>&copy;{new Date().getFullYear()} Vaultic</p>
+      <div className="w-full max-w-screen-lg relative py-5 flex flex-col sm:flex-row justify-between items-center gap-4 text-center sm:text-left">
+        <p>&copy;{new Date().getFullYear()} Vaultic. All rights reserved.</p>
         <p>
-          Designed and Developed by{" "}
+          Designed & developed by{" "}
           <Link
             href="https://www.linkedin.com/in/mohammadbilalmansuri"
             target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Mohammad Bilal Mansuri's LinkedIn Profile"
             className="heading-color leading-none border-b border-transparent hover:border-current transition-colors duration-300"
           >
             Mohammad Bilal Mansuri
