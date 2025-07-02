1a9e20e39ea02e1abaf16c5736d37436a6ea0da0
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-02 15:33:24 +0530

    feat: restructure setup components and update imports in SetupPage

--- src/components/setup/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as ChoosePath } from "./ChoosePath";
-export { default as CreatePassword } from "./CreatePassword";
-export { default as ShowRecoveryPhrase } from "./ShowRecoveryPhrase";
-export { default as EnterRecoveryPhrase } from "./EnterRecoveryPhrase";
-export { default as SettingUpWallet } from "./SettingUpWallet";
-export { default as SetupComplete } from "./SetupComplete";
