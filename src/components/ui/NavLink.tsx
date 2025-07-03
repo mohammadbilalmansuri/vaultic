69315090dcc0368426ad6baa8b12784fb7698790
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-07-03 21:34:26 +0530

    fix: update NavLink component styles for improved layout and className support

--- src/components/ui/NavLink.tsx
+++ src/components/ui/NavLink.tsx
@@ -7,18 +7,26 @@ interface NavLinkProps extends LinkProps {
   children: ReactNode;
   active: boolean;
   target?: HTMLAttributeAnchorTarget;
+  className?: string;
 }
 
-const NavLink = ({ children, href, active, ...props }: NavLinkProps) => {
+const NavLink = ({
+  children,
+  href,
+  active,
+  className = "",
+  ...props
+}: NavLinkProps) => {
   return (
     <Link
       href={href}
       className={cn(
-        "leading-none sm:min-h-10 min-h-9 px-3 py-2.5 rounded-xl flex items-center justify-center transition-all duration-300",
+        "leading-none sm:min-h-10 min-h-8 sm:px-3 px-2.5 py-2.5 rounded-xl flex items-center justify-center transition-all duration-300",
         {
           "bg-primary heading-color cursor-default pointer-events-none": active,
           "bg-transparent hover:heading-color hover:bg-primary": !active,
-        }
+        },
+        className
       )}
       aria-current={active ? "page" : undefined}
       {...props}
