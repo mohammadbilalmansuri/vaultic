777c30325100ed14f4d588eaeb1e3401573d40ff
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-07-03 15:33:26 +0530

    fix: remove unnecessary wrapper div from Switch component for cleaner structure

--- src/components/ui/Switch.tsx
+++ src/components/ui/Switch.tsx
@@ -16,34 +16,29 @@ const Switch = ({
   ...props
 }: SwitchProps) => {
   return (
-    <div className="min-w-fit relative">
-      <button
-        type="button"
-        role="switch"
-        aria-checked={state}
-        aria-label={label}
-        className={cn(
-          "p-1 rounded-full flex items-center bg-secondary",
-          { "h-5 w-9": size === "sm", "h-6 w-12": size === "md" },
-          className
-        )}
-        {...props}
-      >
-        <span
-          className={cn(
-            "block rounded-full transition-transform duration-300",
-            {
-              "size-3": size === "sm",
-              "size-4": size === "md",
-              "translate-x-4": size === "sm" && state,
-              "translate-x-6": size === "md" && state,
-              "bg-teal-500": state,
-              "translate-x-0 bg-zinc-500 dark:bg-zinc-400": !state,
-            }
-          )}
-        />
-      </button>
-    </div>
+    <button
+      type="button"
+      role="switch"
+      aria-checked={state}
+      aria-label={label}
+      className={cn(
+        "shrink-0 relative p-1 rounded-full flex items-center bg-secondary",
+        { "h-5 w-9": size === "sm", "h-6 w-12": size === "md" },
+        className
+      )}
+      {...props}
+    >
+      <span
+        className={cn("block rounded-full transition-transform duration-300", {
+          "size-3": size === "sm",
+          "size-4": size === "md",
+          "translate-x-4": size === "sm" && state,
+          "translate-x-6": size === "md" && state,
+          "bg-teal-500": state,
+          "translate-x-0 bg-zinc-500 dark:bg-zinc-400": !state,
+        })}
+      />
+    </button>
   );
 };
 
