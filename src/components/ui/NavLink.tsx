d96fd96d0ebdf122ae62ebf8615bad5613684e39
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-03 16:03:14 +0530

    fix: adjust padding and height for improved layout consistency in NavLink component

--- src/components/ui/NavLink.tsx
+++ src/components/ui/NavLink.tsx
@@ -14,7 +14,7 @@ const NavLink = ({ children, href, active, ...props }: NavLinkProps) => {
     <Link
       href={href}
       className={cn(
-        "leading-none h-10 sm:px-3 px-2.5 rounded-xl flex items-center justify-center transition-all duration-300",
+        "leading-none sm:min-h-10 min-h-9 px-3 py-2.5 rounded-xl flex items-center justify-center transition-all duration-300",
         {
           "bg-primary heading-color cursor-default pointer-events-none": active,
           "bg-transparent hover:heading-color hover:bg-primary": !active,
