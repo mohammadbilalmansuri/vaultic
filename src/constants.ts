20594c3e1c20d0bf9752415147661636375aa182
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-04 11:15:15 +0530

    fix: add IN_VIEW_OPTIONS constant for improved configuration of view options

--- src/constants.ts
+++ src/constants.ts
@@ -7,3 +7,8 @@ export const BALANCE_DISPLAY_DECIMALS = 4;
 export const FAUCET_PRESET_AMOUNTS = ["0.5", "1", "2.5", "5"];
 
 export const TRANSACTION_LIMIT = 10;
+
+export const IN_VIEW_OPTIONS = {
+  once: true,
+  amount: 0.25,
+} as const;
