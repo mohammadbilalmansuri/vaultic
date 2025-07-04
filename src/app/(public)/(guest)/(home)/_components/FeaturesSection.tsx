9008526cfbe62c116533399249ef4519b792490f
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-07-04 11:15:07 +0530

    fix: refactor FeaturesSection to improve structure and readability by introducing IFeature interface and FeatureCard component

--- src/app/(public)/(guest)/(home)/_components/FeaturesSection.tsx
+++ src/app/(public)/(guest)/(home)/_components/FeaturesSection.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { motion } from "motion/react";
+import { IN_VIEW_OPTIONS } from "@/constants";
+import { TIcon } from "@/types";
 import { fadeUpAnimation } from "@/utils/animations";
-import { useMatchMedia, useMotionInView } from "@/hooks";
+import { useMotionInView } from "@/hooks";
 import {
   ShieldTick,
   Wallet,
@@ -11,81 +13,90 @@ import {
   Code,
 } from "@/components/ui/icons";
 
-const features = [
+interface IFeature {
+  Icon: TIcon;
+  title: string;
+  description: string;
+}
+
+const features: IFeature[] = [
   {
-    icon: ShieldTick,
+    Icon: ShieldTick,
     title: "Fully Encrypted, Fully Local",
     description:
       "Vaultic runs entirely in your browser. Your recovery phrase and wallets are encrypted using AES-GCM and never leave your device — ever.",
   },
   {
-    icon: Wallet,
+    Icon: Wallet,
     title: "One Phrase. Infinite Possibilities.",
     description:
       "Generate multiple Solana and Ethereum accounts from a single seed phrase. No need to manage multiple wallets — everything stays in sync.",
   },
   {
-    icon: Send,
+    Icon: Send,
     title: "Frictionless ETH & SOL Transfers",
     description:
       "Send and receive assets on mainnet and testnet instantly. No browser extensions, no network configs — just type, confirm, and go.",
   },
   {
-    icon: WalletMoney,
+    Icon: WalletMoney,
     title: "Test with Real Tools, Not Real Risk",
     description:
       "Claim free testnet ETH and SOL directly in the interface. Perfect for development, debugging, and demos without touching real funds.",
   },
   {
-    icon: WalletRecovery,
+    Icon: WalletRecovery,
     title: "Recoverable Anywhere",
     description:
       "Your recovery phrase is all you need. Re-import your wallets on any device — Vaultic will regenerate every account deterministically.",
   },
   {
-    icon: Code,
+    Icon: Code,
     title: "100% Open Source & Auditable",
     description:
       "Every line of Vaultic is public and verifiable. Whether you’re a dev or a security expert, you’re welcome to inspect, fork, and improve it.",
   },
 ];
 
+const FeatureCard = ({ Icon, title, description }: IFeature) => {
+  const { ref, inView } = useMotionInView<HTMLDivElement>(IN_VIEW_OPTIONS);
+
+  return (
+    <motion.div
+      ref={ref}
+      className="flex flex-col gap-3 lg:p-8 sm:p-5.5 p-4.5 rounded-3xl border-1.5 border-color"
+      {...fadeUpAnimation({ inView })}
+    >
+      <div className="flex items-center lg:gap-4 md:gap-3.5 gap-2.5">
+        <Icon className="lg:w-8 md:w-7 w-6 text-teal-500" aria-hidden="true" />
+        <h3 className="h3">{title}</h3>
+      </div>
+      <p>{description}</p>
+    </motion.div>
+  );
+};
+
 const FeaturesSection = () => {
-  const isLowHeight = useMatchMedia("(max-height: 800px)");
-  const { ref, inView } = useMotionInView({
-    once: true,
-    amount: isLowHeight ? 0.1 : 0.25,
-  });
+  const { ref, inView } = useMotionInView<HTMLHeadingElement>(IN_VIEW_OPTIONS);
 
   return (
     <section
-      ref={ref}
       role="region"
       aria-label="Features section"
       className="w-full relative flex flex-col items-center lg:gap-8 md:gap-7 sm:gap-6 xs:gap-5 gap-4"
     >
-      <motion.h2 className="h2 text-center" {...fadeUpAnimation({ inView })}>
+      <motion.h2
+        ref={ref}
+        className="h2 text-center"
+        {...fadeUpAnimation({ inView })}
+      >
         Everything You Need.{" "}
         <span className="text-nowrap">Nothing You Don’t.</span>
       </motion.h2>
 
       <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:gap-5 sm:gap-4.5 gap-3.5">
-        {features.map(({ icon: Icon, title, description }, index) => (
-          <motion.div
-            key={`feature-${index}`}
-            className="flex flex-col md:gap-4 gap-3 lg:p-8 sm:p-5.5 p-4.5 rounded-3xl border-1.5 border-color"
-            {...fadeUpAnimation({ inView, delay: index * 0.05 + 0.05 })}
-          >
-            <div className="flex items-center lg:gap-4 md:gap-3.5 gap-2.5">
-              <Icon
-                className="lg:w-8 md:w-7 w-6 text-teal-500"
-                aria-hidden="true"
-                focusable="false"
-              />
-              <h3 className="h3">{title}</h3>
-            </div>
-            <p>{description}</p>
-          </motion.div>
+        {features.map((feature, index) => (
+          <FeatureCard key={`feature-${index}`} {...feature} />
         ))}
       </div>
     </section>
