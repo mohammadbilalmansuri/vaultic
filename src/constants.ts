b2aca573d29798a85696a67ffc41a8090426055c
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-04 11:30:11 +0530

    fix: update IN_VIEW_OPTIONS type and margin for improved configuration

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
+import { UseInViewOptions } from "motion/react";
 import { TNetwork } from "./types";
 
 export const DEFAULT_NETWORK: TNetwork = "ethereum";
@@ -8,7 +9,7 @@ export const FAUCET_PRESET_AMOUNTS = ["0.5", "1", "2.5", "5"];
 
 export const TRANSACTION_LIMIT = 10;
 
-export const IN_VIEW_OPTIONS = {
+export const IN_VIEW_OPTIONS: UseInViewOptions = {
   once: true,
-  amount: 0.25,
+  margin: "0px 0px -10% 0px",
 } as const;
