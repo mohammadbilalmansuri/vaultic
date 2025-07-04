5e9d7f93d9d9ce5427858c4dd5cf054b19de1a58
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-04 07:17:10 +0530

    fix: update HeroSection and HomePage styles for improved layout and responsiveness

--- src/app/(public)/(guest)/(home)/page.tsx
+++ src/app/(public)/(guest)/(home)/page.tsx
@@ -7,7 +7,10 @@ import FAQSection from "./_components/FAQSection";
 
 const HomePage = () => {
   return (
-    <div className="w-full max-w-screen-lg relative flex-1 flex flex-col items-center gap-24 pt-8 pb-16">
+    <div
+      className="w-full max-w-screen-lg relative flex-1 flex flex-col items-center lg:gap-24 md:gap-20 gap-16 sm:pt-3 pt-2 lg:pb-16 md:pb-12 pb-8"
+      aria-label="Home Page Content"
+    >
       <HeroSection />
       <FeaturesSection />
       <HowItWorksSection />
