988f76d396b905cf1f6e823f3f9c235832ad000b
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-04 10:54:35 +0530

    fix: refactor FeaturesSection to use useMotionInView and useMatchMedia for improved responsiveness and readability

--- src/app/(public)/(guest)/(home)/_components/FeaturesSection.tsx
+++ src/app/(public)/(guest)/(home)/_components/FeaturesSection.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { useRef } from "react";
-import { motion, useInView } from "motion/react";
+import { motion } from "motion/react";
 import { fadeUpAnimation } from "@/utils/animations";
+import { useMatchMedia, useMotionInView } from "@/hooks";
 import {
   ShieldTick,
   Wallet,
@@ -51,30 +51,34 @@ const features = [
 ];
 
 const FeaturesSection = () => {
-  const ref = useRef(null);
-  const inView = useInView(ref, { once: true, amount: 0.25 });
+  const isLowHeight = useMatchMedia("(max-height: 800px)");
+  const { ref, inView } = useMotionInView({
+    once: true,
+    amount: isLowHeight ? 0.1 : 0.25,
+  });
 
   return (
     <section
       ref={ref}
       role="region"
       aria-label="Features section"
-      className="w-full relative flex flex-col items-center gap-8"
+      className="w-full relative flex flex-col items-center lg:gap-8 md:gap-7 sm:gap-6 xs:gap-5 gap-4"
     >
-      <motion.h2 className="h2" {...fadeUpAnimation({ inView })}>
-        Everything You Need. Nothing You Don’t.
+      <motion.h2 className="h2 text-center" {...fadeUpAnimation({ inView })}>
+        Everything You Need.{" "}
+        <span className="text-nowrap">Nothing You Don’t.</span>
       </motion.h2>
 
-      <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-5">
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:gap-5 sm:gap-4.5 gap-3.5">
         {features.map(({ icon: Icon, title, description }, index) => (
           <motion.div
             key={`feature-${index}`}
-            className="flex flex-col gap-4 p-8 rounded-3xl border-1.5 border-color"
+            className="flex flex-col md:gap-4 gap-3 lg:p-8 sm:p-5.5 p-4.5 rounded-3xl border-1.5 border-color"
             {...fadeUpAnimation({ inView, delay: index * 0.05 + 0.05 })}
           >
-            <div className="flex items-center gap-4">
+            <div className="flex items-center lg:gap-4 md:gap-3.5 gap-2.5">
               <Icon
-                className="w-8 text-teal-500"
+                className="lg:w-8 md:w-7 w-6 text-teal-500"
                 aria-hidden="true"
                 focusable="false"
               />
