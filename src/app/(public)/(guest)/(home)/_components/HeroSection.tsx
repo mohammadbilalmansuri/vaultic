981c32edf99c83bb8748dca583fc61235593ee77
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-07-04 07:17:10 +0530

    fix: update HeroSection and HomePage styles for improved layout and responsiveness

--- src/app/(public)/(guest)/(home)/_components/HeroSection.tsx
+++ src/app/(public)/(guest)/(home)/_components/HeroSection.tsx
@@ -16,13 +16,13 @@ const HeroSection = () => {
       className="hero"
       {...fadeUpAnimation({ inView })}
     >
-      <p className="bg-teal-500/10 dark:bg-teal-500/5 text-teal-600 dark:text-teal-500 px-2.5 py-2 text-sm rounded-lg font-medium leading-none border border-teal-500/10 dark:border-teal-500/5">
+      <p className="min-w-max sm:px-2.5 sm:py-2 px-2 py-1.5 sm:text-sm text-xs rounded-lg font-medium leading-none border border-teal-500/10 dark:border-teal-500/5 bg-teal-500/10 dark:bg-teal-500/5 text-teal-600 dark:text-teal-500">
         No Extensions &#8226; No Servers &#8226; 100% Yours
       </p>
 
       <h1 className="h1">The Wallet That Lives in Your Browser</h1>
 
-      <p className="text-lg max-w-3xl -mt-2 mb-1">
+      <p className="md:text-lg sm:text-md text-base md:max-w-3xl sm:-mt-2 -mt-1.5 mb-1">
         Vaultic is a secure, browser-native crypto wallet for Solana and
         Ethereum. Generate and manage multiple accounts from a single recovery
         phrase — fully encrypted, open source, and under your complete control.
