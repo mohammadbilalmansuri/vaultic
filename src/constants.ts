ac8150d46cdd9ef96b30aae0fd7457f1087ad432
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-07-04 12:01:18 +0530

    fix: remove unused IN_VIEW_OPTIONS constant to clean up code

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,3 @@
-import { UseInViewOptions } from "motion/react";
 import { TNetwork } from "./types";
 
 export const DEFAULT_NETWORK: TNetwork = "ethereum";
@@ -8,8 +7,3 @@ export const BALANCE_DISPLAY_DECIMALS = 4;
 export const FAUCET_PRESET_AMOUNTS = ["0.5", "1", "2.5", "5"];
 
 export const TRANSACTION_LIMIT = 10;
-
-export const IN_VIEW_OPTIONS: UseInViewOptions = {
-  once: true,
-  margin: "0px 0px -10% 0px",
-} as const;
