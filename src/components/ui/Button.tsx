53cd3b19c4decb667978c52518e494aff17fc5b7
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-07-05 15:48:27 +0530

    fix: adjust gap value in Button component for improved layout

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -31,7 +31,7 @@ const Button = ({
   ...props
 }: ButtonProps) => {
   const classes = cn(
-    "font-medium transition-all duration-300 flex items-center justify-center gap-2.5 rounded-2xl hover:rounded-4xl sm:h-13 h-12 sm:px-5 px-4",
+    "font-medium transition-all duration-300 flex items-center justify-center gap-2 rounded-2xl hover:rounded-4xl sm:h-13 h-12 sm:px-5 px-4",
     {
       "text-zinc-800 bg-teal-500": variant === "teal",
       "heading-color bg-secondary": variant === "zinc",
