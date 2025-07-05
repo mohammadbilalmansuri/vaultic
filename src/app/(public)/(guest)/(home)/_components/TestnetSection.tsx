500a7db0b91fc645529a48671594671312a55ece
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-07-05 18:21:27 +0530

    fix: refactor TestnetSection component for improved structure and readability

--- src/app/(public)/(guest)/(home)/_components/TestnetSection.tsx
+++ src/app/(public)/(guest)/(home)/_components/TestnetSection.tsx
@@ -1,78 +1,83 @@
 "use client";
-import { useRef } from "react";
-import { motion, useInView } from "motion/react";
+import { motion } from "motion/react";
+import Link from "next/link";
 import { fadeUpAnimation } from "@/utils/animations";
-import { Button } from "@/components/ui";
-import { Swap, WalletMoney } from "@/components/ui/icons";
+import { useMotionInView } from "@/hooks";
+import { Tooltip } from "@/components/ui";
+import { ArrowRight } from "@/components/ui/icons";
 
 const content = [
   {
     title: "Testnet Mode",
-    paragraphs: [
-      "Explore Vaultic with full functionality — safely. Testnet Mode mirrors your real wallet on Solana Devnet and Ethereum Sepolia, so you can experiment without risking real assets.",
-      "Switch instantly — no RPC setup or configuration required. Perfect for learning, testing, or debugging before going live.",
-    ],
-    button: {
-      text: "Set Up Wallet & Use Testnet",
-      href: "/setup",
-    },
-    icon: Swap,
+    paragraphs: (
+      <>
+        <p>
+          Explore Vaultic with full functionality — safely and risk-free.
+          Testnet Mode creates a mirror of your real wallet using Solana Devnet
+          and Ethereum Sepolia, so you can test, learn, and experiment without
+          touching real assets.
+        </p>
+        <p>
+          No setup. No configuration. Just switch instantly. It’s ideal for
+          learning, debugging, or trial runs before going live.
+        </p>
+      </>
+    ),
+    button: { text: "Set Up Wallet & Use Testnet", href: "/setup" },
   },
   {
     title: "Get Testnet Tokens",
-    paragraphs: [
-      "Claim free SOL (Devnet) and ETH (Sepolia) to simulate transactions, test dApps, or explore blockchain basics — no wallet setup required.",
-      "Vaultic includes a built-in faucet for Solana and a verified link to the official Sepolia faucet. Open to everyone — developers, testers, and curious beginners.",
-    ],
-    button: {
-      text: "Visit Faucet",
-      href: "/faucet",
-    },
-    icon: WalletMoney,
+    paragraphs: (
+      <>
+        <p>
+          Get free test tokens — no wallet setup needed. Claim SOL (Devnet) and
+          ETH (Sepolia) to try transactions, test dApps, or just explore how
+          blockchain works.
+        </p>
+        <p>
+          Vaultic includes a built-in Solana faucet and a trusted link to the
+          official Sepolia one. Open to everyone — developers, testers, and
+          curious beginners alike.
+        </p>
+      </>
+    ),
+    button: { text: "Visit Faucet", href: "/faucet" },
   },
 ];
 
 const TestnetSection = () => {
-  const ref = useRef(null);
-  const inView = useInView(ref, { once: true, amount: 0.25 });
+  const { ref, inView } = useMotionInView();
 
   return (
     <section
       ref={ref}
       role="region"
       aria-label="Testnet features"
-      className="w-full relative flex flex-col items-center gap-8"
+      className="w-full relative flex flex-col items-center lg:gap-8 md:gap-7 sm:gap-6 gap-5"
     >
-      <motion.h2 className="h2" {...fadeUpAnimation({ inView })}>
+      <motion.h2 className="h2 text-center" {...fadeUpAnimation({ inView })}>
         Test Freely. Learn Safely.
       </motion.h2>
 
-      <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-5">
-        {content.map(({ title, paragraphs, button, icon: Icon }, index) => (
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 sm:gap-5 gap-4">
+        {content.map(({ title, paragraphs, button }, index) => (
           <motion.div
             key={index}
             aria-label={title}
-            className="w-full relative overflow-hidden flex flex-col items-start gap-4 p-8 border-1.5 border-color rounded-3xl"
+            className="w-full relative border-1.5 border-color rounded-3xl flex flex-col items-start sm:gap-2 gap-1.5 sm:pl-6 sm:pb-6 sm:pt-4 sm:pr-4 pl-5 pb-5 pt-3.5 pr-3.5"
             {...fadeUpAnimation({ inView, delay: index * 0.05 + 0.05 })}
           >
-            <h3 className="h3 -mt-1">{title}</h3>
-            {paragraphs.map((paragraph, j) => (
-              <p key={j}>{paragraph}</p>
-            ))}
-            <Button
-              as="link"
-              href={button.href}
-              className="mt-2"
-              variant="zinc"
-            >
-              {button.text}
-            </Button>
-            <span
-              className="absolute right-0 bottom-0 size-24 rounded-tl-full flex items-end justify-end pr-4 pb-4 bg-primary"
-              aria-hidden="true"
-            >
-              <Icon className="w-10 text-zinc-500" />
-            </span>
+            <div className="w-full relative flex items-center-safe justify-between gap-3">
+              <h3 className="h3">{title}</h3>
+              <Tooltip content={button.text} position="left">
+                <Link href={button.href} className="icon-btn-bg">
+                  <ArrowRight />
+                </Link>
+              </Tooltip>
+            </div>
+            <div className="w-full relative flex flex-col sm:gap-3 gap-2.5 pr-2">
+              {paragraphs}
+            </div>
           </motion.div>
         ))}
       </div>
