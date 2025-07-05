203243d0696a39ada3ac66585f97679a7e02aa2b
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-07-05 15:50:50 +0530

    fix: refactor HeroSection component for improved readability and structure

--- src/app/(public)/(open)/help-and-support/_components/HeroSection.tsx
+++ src/app/(public)/(open)/help-and-support/_components/HeroSection.tsx
@@ -1,35 +1,36 @@
 "use client";
-import { useRef } from "react";
-import { motion, useInView } from "motion/react";
+import { motion } from "motion/react";
 import { fadeUpAnimation } from "@/utils/animations";
+import { useMotionInView } from "@/hooks";
 import { Button } from "@/components/ui";
 import { ExternalLink } from "@/components/ui/icons";
 
 const HeroSection = () => {
-  const ref = useRef(null);
-  const inView = useInView(ref, { once: true });
+  const { ref, inView } = useMotionInView();
 
   return (
-    <motion.section ref={ref} className="hero" {...fadeUpAnimation({ inView })}>
-      <motion.h1 className="h1" {...fadeUpAnimation({ delay: 0.1, inView })}>
-        Help & Support
-      </motion.h1>
-
-      <motion.p
-        className="hero-paragraph"
-        {...fadeUpAnimation({ delay: 0.2, inView })}
-      >
+    <motion.section
+      ref={ref}
+      role="region"
+      aria-label="Hero Section"
+      className="hero"
+      {...fadeUpAnimation({ inView })}
+    >
+      <h1 className="h1 -mt-2">Help & Support</h1>
+      <p className="md:text-lg sm:text-md text-base md:max-w-3xl sm:-mt-2 -mt-1.5 mb-1">
         This page has guides and FAQs to help you make the most of Vaultic. If
         you have a unique issue, open a GitHub issue for public tracking or
         contact the admin via email on their GitHub profile for urgent matters.
-      </motion.p>
-
-      <motion.div {...fadeUpAnimation({ delay: 0.3, inView })}>
-        <Button as="link" href="/setup">
-          <ExternalLink className="w-4.5" />
-          Open GitHub Issue
-        </Button>
-      </motion.div>
+      </p>
+      <Button
+        as="link"
+        href="/setup"
+        aria-label="Open GitHub Issue"
+        className="gap-2"
+      >
+        <ExternalLink className="w-4.5" />
+        Open GitHub Issue
+      </Button>
     </motion.section>
   );
 };
