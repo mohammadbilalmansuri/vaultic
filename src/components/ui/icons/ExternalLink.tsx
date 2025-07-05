56f8beaf290bd8786b6ba1e227aca64457f7c7fc
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-05 15:50:31 +0530

    fix: rename component from OpenLink to ExternalLink for consistency

--- src/components/ui/icons/ExternalLink.tsx
+++ src/components/ui/icons/ExternalLink.tsx
@@ -1,6 +1,6 @@
 import { TIcon } from "@/types";
 
-const OpenLink: TIcon = (props) => {
+const ExternalLink: TIcon = (props) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -19,4 +19,4 @@ const OpenLink: TIcon = (props) => {
   );
 };
 
-export default OpenLink;
+export default ExternalLink;
