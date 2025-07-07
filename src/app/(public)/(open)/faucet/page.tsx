e06864ee448234f4acc3ba6025b25c0f6b73c8be
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-07 22:46:48 +0530

    fix: update structure of FaucetPage for improved semantics and accessibility

--- src/app/(public)/(open)/faucet/page.tsx
+++ src/app/(public)/(open)/faucet/page.tsx
@@ -6,39 +6,41 @@ import { Solana, Ethereum, Logo } from "@/components/ui/icons";
 
 const FaucetPage = () => {
   return (
-    <div className="box-page" aria-label="Faucet Page">
-      <motion.div className="box without-progress" {...scaleUpAnimation()}>
-        <Logo className="box-icon text-teal-500" aria-hidden="true" />
-        <h1 className="xxs:text-nowrap">Claim Free Testnet Tokens</h1>
-        <p className="-mt-2.5">
-          Get free ETH (Sepolia) and SOL (Devnet) for testing, development, or
-          exploring blockchain features. Perfect for simulating transactions and
-          experimenting safely on testnets.
-        </p>
-        <div className="flex flex-col items-center sm:gap-4 gap-3">
-          <Button
-            variant="zinc"
-            as="link"
-            href="https://cloud.google.com/application/web3/faucet/ethereum/sepolia"
-            rel="noopener noreferrer"
-            target="_blank"
-            className="w-full"
-          >
-            <Ethereum className="sm:h-6 h-5.5 shrink-0" />
-            <span className="mt-px">Ethereum Sepolia</span>
-          </Button>
-          <Button
-            variant="zinc"
-            as="link"
-            href="/faucet/solana"
-            className="w-full"
-          >
-            <Solana className="sm:h-4 h-3.5 shrink-0" />
-            <span className="mt-px">Solana Devnet</span>
-          </Button>
-        </div>
-      </motion.div>
-    </div>
+    <motion.div
+      aria-label="Faucet Resources"
+      className="box without-progress"
+      {...scaleUpAnimation()}
+    >
+      <Logo className="box-icon text-teal-500" aria-hidden="true" />
+      <h1 className="xxs:text-nowrap">Claim Free Testnet Tokens</h1>
+      <p className="-mt-2.5">
+        Get free ETH (Sepolia) and SOL (Devnet) for testing, development, or
+        exploring blockchain features. Perfect for simulating transactions and
+        experimenting safely on testnets.
+      </p>
+      <div className="flex flex-col items-center sm:gap-4 gap-3">
+        <Button
+          variant="zinc"
+          as="link"
+          href="https://cloud.google.com/application/web3/faucet/ethereum/sepolia"
+          rel="noopener noreferrer"
+          target="_blank"
+          className="w-full"
+        >
+          <Ethereum className="sm:h-6 h-5.5 shrink-0" />
+          <span className="mt-px">Ethereum Sepolia</span>
+        </Button>
+        <Button
+          variant="zinc"
+          as="link"
+          href="/faucet/solana"
+          className="w-full"
+        >
+          <Solana className="sm:h-4 h-3.5 shrink-0" />
+          <span className="mt-px">Solana Devnet</span>
+        </Button>
+      </div>
+    </motion.div>
   );
 };
 
