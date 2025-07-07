48fddd0bb772598dcded254daa90472c8350f8c3
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-07 22:03:49 +0530

    fix: update layout and semantics in SetupComplete component for improved accessibility and user experience

--- src/app/(public)/(guest)/setup/_components/SetupComplete.tsx
+++ src/app/(public)/(guest)/setup/_components/SetupComplete.tsx
@@ -9,18 +9,19 @@ const SetupComplete = ({ path }: { path: TSetupPath }) => {
   return (
     <motion.div
       key="setup-complete"
+      aria-label="Setup Complete Step"
+      className="box without-progress"
       {...scaleUpAnimation({ duration: 0.15 })}
-      className="box gap-6 xs:py-12 xs:px-12 py-10 px-8"
     >
       <Logo className="box-icon text-teal-500" aria-hidden="true" />
-      <h2>Your wallet is ready</h2>
-      <p className="-mt-2">
+      <h1>Your wallet is ready</h1>
+      <p className="-mt-2.5">
         You’ve successfully {path === "create" ? "created" : "imported"} your
         wallet. You’re now ready to explore, send, and manage your assets
         securely.
       </p>
       <Button as="link" href="/dashboard">
-        Enter Dashboard
+        Go to Dashboard
       </Button>
     </motion.div>
   );
