f1e29cc52c1450ddc3d7830833c63137d22158e5
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-07-07 22:59:31 +0530

    fix: refine layout of OfflineView for improved spacing and consistency

--- src/components/layout/OfflineView.tsx
+++ src/components/layout/OfflineView.tsx
@@ -8,12 +8,12 @@ const OfflineView = () => {
     <motion.div
       role="alert"
       aria-label="Connection Lost Alert"
-      className="box without-progress sm:mt-3 sm:mb-4 mt-2 mb-3"
+      className="box without-progress"
       {...scaleUpAnimation()}
     >
       <WiFiOff className="box-icon text-yellow-500" aria-hidden="true" />
       <h1>Connection Lost</h1>
-      <p className="max-w-xs -mt-2">
+      <p className="max-w-xs -mt-2.5">
         Please check your internet connection and try again.
       </p>
     </motion.div>
