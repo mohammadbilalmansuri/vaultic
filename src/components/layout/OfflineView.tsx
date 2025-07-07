62f7c3c8fccadb5a216a28779bf48f783f368125
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-07 16:59:01 +0530

    fix: update layout classes in OfflineView for improved consistency and readability

--- src/components/layout/OfflineView.tsx
+++ src/components/layout/OfflineView.tsx
@@ -8,7 +8,7 @@ const OfflineView = () => {
     <motion.div
       role="alert"
       aria-label="Connection Lost Alert"
-      className="box without-progress"
+      className="box without-progress sm:mt-3 sm:mb-4 mt-2 mb-3"
       {...scaleUpAnimation()}
     >
       <WiFiOff className="box-icon text-yellow-500" aria-hidden="true" />
