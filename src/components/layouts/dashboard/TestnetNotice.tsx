12ea9c6122bf2ad12e3bcf28d665cd53696f685b
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-09 22:42:33 +0530

    refactor: remove Tooltip wrapper from TestnetNotice component and adjust styles

--- src/components/layouts/dashboard/TestnetNotice.tsx
+++ src/components/layouts/dashboard/TestnetNotice.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { Tooltip } from "@/components/ui";
 import { useWalletStore } from "@/stores";
 
 const TestnetNotice = () => {
@@ -8,11 +7,13 @@ const TestnetNotice = () => {
   if (networkMode !== "testnet") return null;
 
   return (
-    <Tooltip content="You are currently in testnet mode." position="bottom">
-      <div className="bg-yellow-500/10 text-yellow-800 dark:text-yellow-500 border border-yellow-500/30 dark:border-yellow-500/10 uppercase text-sm font-medium leading-none p-2 rounded-lg select-none cursor-default">
-        Testnet Mode
-      </div>
-    </Tooltip>
+    <div
+      role="status"
+      aria-label="Testnet Mode Indicator"
+      className="bg-warning text-warning border border-warning uppercase text-sm font-medium leading-none p-2 rounded-lg select-none cursor-default"
+    >
+      Testnet Mode
+    </div>
   );
 };
 
