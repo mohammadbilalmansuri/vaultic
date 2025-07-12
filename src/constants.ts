720d961ee10e463da68d881265b7117389dfd735
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-12 08:06:24 +0530

    refactor: update navigation sidebar icons for consistency

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import { TNetwork } from "./types";
 import {
+  Home,
   Wallet,
-  Cards,
   Setting,
   WalletMoney,
   QuestionMark,
@@ -24,8 +24,8 @@ export const NAVIGATION_HEADER = (walletExists: boolean) => [
 ];
 
 export const NAVIGATION_SIDEBAR = [
-  { name: "Dashboard", href: "/dashboard", Icon: Wallet },
-  { name: "Accounts", href: "/accounts", Icon: Cards },
+  { name: "Dashboard", href: "/dashboard", Icon: Home },
+  { name: "Accounts", href: "/accounts", Icon: Wallet },
   { name: "Settings", href: "/settings", Icon: Setting },
   { name: "Faucet", href: "/faucet", Icon: WalletMoney },
   { name: "Help & Support", href: "/help-and-support", Icon: QuestionMark },
