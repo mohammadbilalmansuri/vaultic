42075a86f64d8fe4da3617fdd0ae8b339fb80c01
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-16 22:05:52 +0530

    fix: adjust padding in DashboardLayout for improved layout consistency

--- src/components/layouts/dashboard/DashboardLayout.tsx
+++ src/components/layouts/dashboard/DashboardLayout.tsx
@@ -8,7 +8,7 @@ const DashboardLayout = ({ children }: Children) => {
       <main
         role="main"
         aria-label="Dashboard Main Content"
-        className="w-full relative flex-1 flex flex-col items-center md:px-5 px-4 lg:pt-6 md:pt-2 pt-3 pb-8 overflow-x-hidden"
+        className="w-full relative flex-1 flex flex-col items-center lg:px-6 md:px-5 px-4 lg:pt-6 md:pt-2 pt-3 lg:pb-12 md:pb-10 pb-8 overflow-x-hidden"
       >
         {children}
       </main>
