40fd044352eaec50b993efd1cbd8fc98ac178fa6
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-17 12:10:08 +0530

    fix: enhance Tabs component styling for improved layout and responsiveness

--- src/components/shared/Tabs.tsx
+++ src/components/shared/Tabs.tsx
@@ -24,15 +24,13 @@ const Tabs = ({ tabs, delay }: TabsProps) => {
   return (
     <div className="w-full relative flex flex-col items-center gap-7">
       <motion.div
-        className="w-full relative bg-primary rounded-2xl h-14 grid items-center"
-        style={{
-          gridTemplateColumns: `repeat(${tabKeys.length}, 1fr)`,
-        }}
-        {...fadeUpAnimation({ delay: delay?.list })}
         role="tablist"
         aria-label="Tabs Navigation"
+        className="w-full relative bg-primary rounded-2xl p-1.5 grid gap-1.5 items-center overflow-x-auto scrollbar-hide"
+        style={{ gridTemplateColumns: `repeat(${tabKeys.length}, 1fr)` }}
+        {...fadeUpAnimation({ delay: delay?.list })}
       >
-        <motion.div
+        {/* <motion.div
           className="absolute bg-secondary rounded-xl h-[calc(100%-10px)]"
           animate={{
             left: `calc(${activeIndex} * ${100 / tabKeys.length}% + 5px)`,
@@ -41,7 +39,7 @@ const Tabs = ({ tabs, delay }: TabsProps) => {
           transition={{ type: "spring", stiffness: 300, damping: 30 }}
           initial={false}
           aria-hidden="true"
-        />
+        /> */}
 
         {tabKeys.map((label, index) => {
           const Icon = tabs[label]?.icon;
@@ -56,16 +54,20 @@ const Tabs = ({ tabs, delay }: TabsProps) => {
               aria-controls={`tabpanel-${label}`}
               id={`tab-${label}`}
               className={cn(
-                "h-full px-3 transition-all duration-200 relative z-10 flex items-center justify-center gap-2 group",
-                isActive
-                  ? "heading-color pointer-events-none"
-                  : "hover:heading-color"
+                "min-w-fit p-3 relative z-10 rounded-xl transition-all duration-200",
+                {
+                  "flex items-center justify-center gap-2": !!Icon,
+                  "bg-secondary heading-color pointer-events-none": isActive,
+                  "hover:heading-color": !isActive,
+                }
               )}
               onClick={() => setActiveTab(label)}
               tabIndex={isActive ? 0 : -1}
             >
-              {Icon && <Icon className="w-5" aria-hidden="true" />}
-              <span className="font-medium leading-snug">{label}</span>
+              {Icon && <Icon className="w-5 shrink-0" aria-hidden="true" />}
+              <span className="font-medium leading-none text-nowrap">
+                {label}
+              </span>
             </button>
           );
         })}
