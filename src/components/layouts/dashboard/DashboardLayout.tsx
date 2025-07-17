130f891c5c0783654311b5ea57fbb824a758423e
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-17 17:02:20 +0530

    feat: integrate TestnetStatus component into DashboardLayout for enhanced visibility

--- src/components/layouts/dashboard/DashboardLayout.tsx
+++ src/components/layouts/dashboard/DashboardLayout.tsx
@@ -1,16 +1,20 @@
 import type { Children } from "@/types";
 import Sidebar from "./Sidebar";
+import { TestnetStatus } from "@/components/shared";
 
 const DashboardLayout = ({ children }: Children) => {
   return (
     <div className="w-full h-dvh max-h-dvh relative flex-1 flex lg:flex-row flex-col overflow-hidden">
       <Sidebar />
       <main
+        className="w-full relative flex flex-col items-center"
         role="main"
         aria-label="Dashboard Main Content"
-        className="w-full relative flex-1 flex flex-col items-center lg:px-6 md:px-5 px-4 lg:pt-6 md:pt-2 pt-3 lg:pb-12 md:pb-10 pb-8 overflow-x-hidden"
       >
-        {children}
+        <TestnetStatus variant="notice" className="w-full lg:block hidden" />
+        <div className="w-full relative flex flex-col items-center overflow-x-hidden lg:px-6 md:px-5 px-4 lg:pt-6 md:pt-2 pt-3 lg:pb-12 md:pb-10 pb-8">
+          {children}
+        </div>
       </main>
     </div>
   );
