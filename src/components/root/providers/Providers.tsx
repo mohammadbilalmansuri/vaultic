e460abe1bbe331121351640ba9671f7b0db88c20
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-07-20 07:09:07 +0530

    refactor: add nextjs-toploader for improved loading experience

--- src/components/root/providers/Providers.tsx
+++ src/components/root/providers/Providers.tsx
@@ -1,4 +1,5 @@
 "use client";
+import NextTopLoader from "nextjs-toploader";
 import type { Children } from "@/types";
 import NotificationProvider from "./NotificationProvider";
 import ThemeProvider from "./ThemeProvider";
@@ -7,6 +8,7 @@ import WalletChecker from "./WalletChecker";
 const Providers = ({ children }: Children) => {
   return (
     <ThemeProvider>
+      <NextTopLoader color="var(--color-teal-500)" showSpinner={false} />
       <WalletChecker>{children}</WalletChecker>
       <NotificationProvider />
     </ThemeProvider>
