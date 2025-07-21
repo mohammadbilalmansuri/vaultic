2a43b85469f14c2f3c6fe1b7b89ec8b2d9d8176e
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-21 08:56:58 +0530

    refactor: update DashboardLayout to use flex-1 for improved layout responsiveness

--- src/components/layouts/dashboard/DashboardLayout.tsx
+++ src/components/layouts/dashboard/DashboardLayout.tsx
@@ -12,7 +12,7 @@ const DashboardLayout = ({ children }: Children) => {
         aria-label="Dashboard Main Content"
       >
         <TestnetStatus variant="notice" className="w-full lg:block hidden" />
-        <div className="w-full relative flex flex-col items-center overflow-x-hidden lg:px-6 md:px-5 px-4 lg:pt-6 md:pt-2 pt-3 lg:pb-12 md:pb-10 pb-8">
+        <div className="w-full relative flex-1 flex flex-col items-center overflow-x-hidden lg:px-6 md:px-5 px-4 lg:pt-6 md:pt-2 pt-3 lg:pb-12 md:pb-10 pb-8">
           {children}
         </div>
       </main>
