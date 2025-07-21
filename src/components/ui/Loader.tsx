7481d03e13a610be36bf3f393d6e205a26f10985
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-21 07:17:46 +0530

    refactor: update Loader color options for improved consistency

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -2,7 +2,7 @@ import cn from "@/utils/cn";
 
 interface LoaderProps {
   size?: "xs" | "sm" | "md" | "lg";
-  color?: "teal" | "white" | "black" | "zinc";
+  color?: "teal" | "zinc" | "current";
 }
 
 const Loader = ({ size = "lg", color = "teal" }: LoaderProps) => {
@@ -16,9 +16,8 @@ const Loader = ({ size = "lg", color = "teal" }: LoaderProps) => {
           "size-8": size === "md",
           "size-10": size === "lg",
           "border-t-teal-500": color === "teal",
-          "border-t-zinc-200": color === "white",
-          "border-t-zinc-800": color === "black",
           "border-t-zinc-800 dark:border-t-zinc-200": color === "zinc",
+          "border-t-current": color === "current",
         }
       )}
       role="status"
