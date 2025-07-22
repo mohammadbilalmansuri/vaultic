ef1521024ba6dfe0f8523555dd4d1624037c707d
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-07-22 16:53:28 +0530

    fix: add overflow-y-auto to main and child div for improved scrolling behavior in DashboardLayout

--- src/components/layouts/dashboard/DashboardLayout.tsx
+++ src/components/layouts/dashboard/DashboardLayout.tsx
@@ -7,12 +7,12 @@ const DashboardLayout = ({ children }: Children) => {
     <div className="w-full h-dvh max-h-dvh relative flex-1 flex lg:flex-row flex-col overflow-hidden">
       <Sidebar />
       <main
-        className="w-full relative flex-1 flex flex-col items-center"
+        className="w-full relative flex-1 flex flex-col items-center overflow-y-auto"
         role="main"
         aria-label="Dashboard Main Content"
       >
         <TestnetStatus variant="notice" className="w-full lg:block hidden" />
-        <div className="w-full relative flex-1 flex flex-col items-center overflow-x-hidden lg:px-6 md:px-5 px-4 lg:pt-6 md:pt-2 pt-3 lg:pb-12 md:pb-10 pb-8">
+        <div className="w-full relative flex-1 flex flex-col items-center lg:px-6 md:px-5 px-4 lg:pt-6 md:pt-2 pt-3 lg:pb-12 md:pb-10 pb-8 overflow-y-auto">
           {children}
         </div>
       </main>
