185da2425af09c2a8d229819af6c10b2bcb4434d
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-24 17:31:22 +0530

    fix: refactor AccountCard component for improved state management and UI consistency

--- src/app/(wallet)/(locked)/accounts/_components/AccountCard.tsx
+++ src/app/(wallet)/(locked)/accounts/_components/AccountCard.tsx
@@ -1,12 +1,13 @@
 "use client";
-import { useState } from "react";
+import { use, useState } from "react";
 import { AnimatePresence, motion } from "motion/react";
-import { NETWORKS } from "@/config";
+import { DEFAULT_NETWORK } from "@/constants";
 import type { Account, Network } from "@/types";
 import { useAccountsStore, useNotificationStore } from "@/stores";
-import { fadeUpAnimation } from "@/utils/animations";
+import { expandCollapseAnimation, fadeUpAnimation } from "@/utils/animations";
 import cn from "@/utils/cn";
-import { useAccounts } from "@/hooks";
+import delay from "@/utils/delay";
+import { useAccounts, useOutsideClick } from "@/hooks";
 import { Trash, Check, Cancel } from "@/components/icons";
 import { Loader, Tooltip } from "@/components/ui";
 import NetworkDetails from "./NetworkDetails";
@@ -28,23 +29,32 @@ const AccountCard = ({
     (state) => state.switchingToAccount
   );
   const notify = useNotificationStore((state) => state.notify);
-  const { switchActiveAccount, deleteAccount } = useAccounts();
 
-  const [removing, setRemoving] = useState<
-    "no" | "want" | "confirm" | "removing"
-  >("no");
+  const [showingNetwork, setShowingNetwork] =
+    useState<Network>(DEFAULT_NETWORK);
+  const [removeState, setRemoveState] = useState<"want" | "removeState" | "no">(
+    "no"
+  );
+
+  const isRemoveConfirmationOpen = removeState === "want";
+
+  const { switchActiveAccount, deleteAccount } = useAccounts();
+  const removeDropdownOutsideClickRef = useOutsideClick<HTMLDivElement>(() => {
+    if (isRemoveConfirmationOpen) setRemoveState("no");
+  }, isRemoveConfirmationOpen);
 
   const isSwitching = switchingToAccount === accountIndex;
 
   const handleAccountRemove = async () => {
-    if (removing !== "confirm") return;
-    setRemoving("removing");
+    if (removeState !== "want") return;
+    setRemoveState("removeState");
 
     try {
+      await delay(1000);
       await deleteAccount(accountIndex);
       notify({
         type: "success",
-        message: `Account ${accountIndex} removed successfully.`,
+        message: `Account ${accountIndex + 1} removed successfully.`,
       });
     } catch {
       notify({
@@ -52,29 +62,51 @@ const AccountCard = ({
         message: "Failed to remove account. Please try again.",
       });
     } finally {
-      setRemoving("no");
+      setRemoveState("no");
     }
   };
 
   return (
     <motion.div
-      className="w-full relative rounded-3xl border-1.5 p-3 flex flex-col gap-3"
+      className="w-full relative rounded-3xl border-1.5 md:p-4 p-3 flex flex-col items-center md:gap-4 gap-3"
       {...fadeUpAnimation({ delay: accountIndex * 0.05 })}
     >
-      <div className="w-full relative flex items-center justify-between gap-3 pl-2">
-        <div className="flex items-center gap-3">
-          <h2 className="sm:text-xl text-lg font-semibold text-primary">
-            Account {accountIndex + 1}
-          </h2>
-          {isActive && (
-            <span className="highlight-teal border sm:text-sm text-xs font-medium uppercase leading-none px-2.5 py-1.5 rounded-lg whitespace-nowrap">
-              Active
-            </span>
-          )}
+      <div className="flex items-center gap-2 bg-default rounded-lg absolute z-10 -top-4.25">
+        <span className="text-primary border-1.5 text-sm font-medium uppercase leading-none px-2.5 py-2 rounded-lg whitespace-nowrap">
+          Account {accountIndex + 1}
+        </span>
+        {isActive && (
+          <span className="text-primary border-1.5 text-sm font-medium uppercase leading-none px-2.5 py-2 rounded-lg whitespace-nowrap">
+            Active
+          </span>
+        )}
+      </div>
+
+      <div className="w-full relative flex items-center justify-between gap-4 md:mt-0 mt-5">
+        <div className="flex items-center gap-2">
+          {Object.keys(account).map((net) => {
+            const network = net as Network;
+            const isSelected = showingNetwork === network;
+            return (
+              <button
+                key={network}
+                type="button"
+                className={cn(
+                  "border h-8 py-2 px-2.5 rounded-lg leading-none capitalize flex items-center justify-center",
+                  isSelected
+                    ? "pointer-events-none highlight-teal"
+                    : "highlight-zinc hover:bg-secondary"
+                )}
+                onClick={() => setShowingNetwork(network)}
+              >
+                {network}
+              </button>
+            );
+          })}
         </div>
 
         {hasMultipleAccounts && (
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-3">
             {(isSwitching || !isActive) && (
               <Tooltip
                 content={isSwitching ? "Switching..." : "Set as Active"}
@@ -94,45 +126,49 @@ const AccountCard = ({
               </Tooltip>
             )}
 
-            <div className="relative flex items-center">
+            <div className="flex flex-col items-end">
               <Tooltip
-                content={removing === "want" ? "Cancel" : "Remove Account"}
+                content={removeState === "want" ? "Cancel" : "Remove Account"}
                 position="left"
               >
                 <button
                   className={cn("icon-btn-bg-sm", {
-                    "hover:text-rose-500": removing !== "no",
+                    "hover:text-rose-500": removeState === "no",
+                    "bg-secondary": removeState === "want",
                   })}
                   onClick={() =>
-                    setRemoving((prev) => (prev === "want" ? "no" : "want"))
+                    setRemoveState((prev) => (prev === "want" ? "no" : "want"))
                   }
                   aria-label={
-                    removing === "want"
+                    removeState === "want"
                       ? "Cancel Account Removal"
                       : "Remove Account"
                   }
                 >
-                  {removing === "want" ? <Cancel /> : <Trash />}
+                  {removeState === "want" ? <Cancel /> : <Trash />}
                 </button>
               </Tooltip>
 
               <AnimatePresence>
-                {removing === "confirm" && (
+                {removeState === "want" && (
                   <motion.div
-                    className="absolute z-50 top-full mt-2 bg-secondary rounded-lg"
-                    initial={{ opacity: 0, scale: 0.8 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    exit={{ opacity: 0, scale: 0.8 }}
+                    className="max-w-60 absolute z-50 top-full mt-2 bg-default border rounded-lg shadow-xl overflow-hidden"
+                    {...expandCollapseAnimation({
+                      duration: 0.15,
+                      ease: "easeOut",
+                    })}
                   >
-                    <div className="flex items-center justify-between text-center p-3">
-                      <p>Confirm Account Removal</p>
+                    <div className="flex flex-col items-center gap-2 text-center p-3 bg-input">
+                      <p className="leading-snug text-15">
+                        Are you sure you want to remove this account?
+                      </p>
                       <button
                         type="button"
-                        className="px-2 py-1 hightlight-rose rounded-lg"
+                        className="px-2 py-1 border highlight-rose rounded-lg text-sm"
                         onClick={handleAccountRemove}
-                        aria-label="Confirm Delete"
+                        aria-label="Remove Account"
                       >
-                        Confirm
+                        Remove
                       </button>
                     </div>
                   </motion.div>
@@ -143,15 +179,7 @@ const AccountCard = ({
         )}
       </div>
 
-      <div className="w-full relative grid grid-cols-2 gap-3">
-        {Object.entries(account).map(([network, details], index) => (
-          <NetworkDetails
-            key={network}
-            network={network as Network}
-            {...details}
-          />
-        ))}
-      </div>
+      <NetworkDetails network={showingNetwork} {...account[showingNetwork]} />
     </motion.div>
   );
 };
