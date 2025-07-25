2139b9c6c85d88909aa86ce0e952b51dd574bd6e
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-25 06:52:13 +0530

    fix: replace apostrophe with HTML entity in NotFound component for consistency

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -26,7 +26,8 @@ const NotFound = () => {
           />
           <h1>Page Not Found</h1>
           <p className="-mt-2.5">
-            The page you're looking for doesn't exist or has been moved.
+            The page you&apos;re looking for doesn&apos;t exist or has been
+            moved.
           </p>
           <Button
             variant="zinc"
