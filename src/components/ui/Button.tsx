06573d03c9fce6122baed9c0d7c5f83003da6ea8
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-07-27 18:11:56 +0530

    refactor: remove size prop from Button component and simplify class handling

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -12,7 +12,6 @@ type ButtonVariant = "teal" | "zinc" | "rose";
 
 interface ButtonBaseProps {
   variant?: ButtonVariant;
-  size?: "sm" | "lg";
   children: ReactNode;
   className?: string;
 }
@@ -33,18 +32,13 @@ type ButtonProps = ButtonAsButton | ButtonAsLink;
 const Button = ({
   as = "button",
   variant = "teal",
-  size = "lg",
   children,
   className = "",
   ...props
 }: ButtonProps) => {
   const classes = cn(
-    "font-medium transition-all duration-200 flex items-center justify-center gap-2",
+    "font-medium transition-all duration-200 flex items-center justify-center gap-2 rounded-2xl hover:rounded-5xl sm:h-13 h-12 py-4 sm:px-5 px-4",
     {
-      "sm:h-13 h-12 py-4 sm:px-5 px-4 rounded-2xl hover:rounded-5xl":
-        size === "lg",
-      "sm:h-11 h-10 py-3 sm:px-4 px-3 rounded-xl hover:rounded-4xl":
-        size === "sm",
       "text-teal-950 bg-teal-500": variant === "teal",
       "text-primary bg-secondary": variant === "zinc",
       "text-rose-50 bg-rose-500": variant === "rose",
