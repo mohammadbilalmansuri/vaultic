7ed8320ed386fd6a43ac7d0c35e9bef74c2b79e1
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-07-27 17:21:40 +0530

    fix: adjust spacing in AccountsPage layout for improved consistency

--- src/app/(wallet)/(locked)/accounts/page.tsx
+++ src/app/(wallet)/(locked)/accounts/page.tsx
@@ -80,7 +80,7 @@ const AccountsPage = () => {
         </Tooltip>
       </motion.div>
 
-      <div className="w-full relative flex flex-col md:gap-12 gap-10 md:mt-2 mt-4">
+      <div className="w-full relative flex flex-col md:gap-6 gap-5">
         {accountEntries.map(([key, account]) => {
           const accountIndex = parseInt(key);
           const isActive = accountIndex === activeAccountIndex;
