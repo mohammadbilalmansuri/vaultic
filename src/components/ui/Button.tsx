379199feeb2b1ffcab7d7e0fa45bd952b5d7a327
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-07-27 11:38:49 +0530

    fix: add 'rose' variant to Button component and adjust class handling for size

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -8,10 +8,11 @@ import type {
 } from "react";
 import cn from "@/utils/cn";
 
-type ButtonVariant = "teal" | "zinc";
+type ButtonVariant = "teal" | "zinc" | "rose";
 
 interface ButtonBaseProps {
   variant?: ButtonVariant;
+  size?: "sm" | "lg";
   children: ReactNode;
   className?: string;
 }
@@ -32,15 +33,21 @@ type ButtonProps = ButtonAsButton | ButtonAsLink;
 const Button = ({
   as = "button",
   variant = "teal",
+  size = "lg",
   children,
   className = "",
   ...props
 }: ButtonProps) => {
   const classes = cn(
-    "font-medium transition-all duration-200 flex items-center justify-center gap-2 rounded-2xl hover:rounded-5xl sm:h-13 h-12 sm:px-5 px-4",
+    "font-medium transition-all duration-200 flex items-center justify-center gap-2",
     {
+      "sm:h-13 h-12 py-4 sm:px-5 px-4 rounded-2xl hover:rounded-5xl":
+        size === "lg",
+      "sm:h-11 h-10 py-3 sm:px-4 px-3 rounded-xl hover:rounded-4xl":
+        size === "sm",
       "text-teal-950 bg-teal-500": variant === "teal",
       "text-primary bg-secondary": variant === "zinc",
+      "text-rose-50 bg-rose-500": variant === "rose",
     },
     className
   );
