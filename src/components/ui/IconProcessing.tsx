ee1f11f2e60a09bfc5fa374dc3f0a6e59aa4a6a6
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-07-31 19:46:31 +0530

    fix: adjust size classes in IconProcessing component for responsive design

--- src/components/ui/IconProcessing.tsx
+++ src/components/ui/IconProcessing.tsx
@@ -7,7 +7,7 @@ const IconProcessing = ({ children }: Children) => {
       role="status"
       aria-label="Processing"
     >
-      <div className="rounded-full size-20 border-3 border-t-teal-500 animate-spin" />
+      <div className="rounded-full xs:size-20 size-18 border-3 border-t-teal-500 animate-spin" />
       <div className="absolute">{children}</div>
     </div>
   );
