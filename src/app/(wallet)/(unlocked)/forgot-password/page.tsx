35c72957aba3482f4e7d2d096f6c6660f8b569f5
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-08-05 18:48:53 +0530

    fix: update titles to use a consistent separator in metadata across multiple pages

--- src/app/(wallet)/(unlocked)/forgot-password/page.tsx
+++ src/app/(wallet)/(unlocked)/forgot-password/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { ForgotPasswordClient } from "./client";
 
 export const metadata: Metadata = {
-  title: "Forgot Password | Vaultic",
+  title: "Forgot Password • Vaultic",
   description:
     "Reset your Vaultic wallet by re-importing it using your recovery phrase. Securely restore access if you’ve forgotten your password.",
   alternates: { canonical: "/forgot-password" },
