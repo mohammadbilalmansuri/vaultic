b4645dc8b71836681e52c94d4e7da164e13ba915
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-08-07 12:50:35 +0530

    refactor: replace custom theme provider with next-themes integration and remove unused theme store

--- src/components/root/providers.tsx
+++ src/components/root/providers.tsx
@@ -1,16 +1,22 @@
 "use client";
 import NextTopLoader from "nextjs-toploader";
+import { ThemeProvider } from "next-themes";
 import type { Children } from "@/types";
 import NotificationProvider from "./providers/notification-provider";
-import ThemeProvider from "./providers/theme-provider";
 import WalletChecker from "./providers/wallet-checker";
 
 const Providers = ({ children }: Children) => {
   return (
-    <ThemeProvider>
-      <NextTopLoader color="var(--color-teal-500)" showSpinner={false} />
-      <WalletChecker>{children}</WalletChecker>
-      <NotificationProvider />
+    <ThemeProvider
+      attribute="class"
+      enableSystem={true}
+      disableTransitionOnChange={true}
+    >
+      <div className="root">
+        <NextTopLoader color="var(--color-teal-500)" showSpinner={false} />
+        <WalletChecker>{children}</WalletChecker>
+        <NotificationProvider />
+      </div>
     </ThemeProvider>
   );
 };
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,6 +1,5 @@
 export * from "./accounts-store";
 export * from "./clipboard-store";
 export * from "./notification-store";
-export * from "./theme-store";
 export * from "./transactions-store";
 export * from "./wallet-store";
